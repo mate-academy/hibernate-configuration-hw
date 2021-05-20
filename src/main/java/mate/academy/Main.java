0e56eeaa1f8717c46535381fd2cbbb7921616ac2
mankokolya <mankokolya@gmail.com>
2021-05-20 15:18:36 +0300

    hibernate configuration task done

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,32 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie gameOfThrones = new Movie("Game Of Thrones", "Awesome fantasy movie");
+        Movie harryPotterAndTheCursedChild =
+                new Movie("Harry Potter and the Cursed Child", "New film about Harry Potter");
+
+        System.out.println("----Movies before saving----");
+        System.out.println(gameOfThrones);
+        System.out.println(harryPotterAndTheCursedChild);
+
+        movieService.add(gameOfThrones);
+        movieService.add(harryPotterAndTheCursedChild);
+
+        System.out.println("-----Movies after saving----");
+        System.out.println(gameOfThrones);
+        System.out.println(harryPotterAndTheCursedChild);
+
+        System.out.println("----Testing get method-----");
+        System.out.println(movieService.get(gameOfThrones.getId()));
+        System.out.println(movieService.get(harryPotterAndTheCursedChild.getId()));
     }
 }
---
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,42 @@
+package mate.academy.dao.impl;
+
+import java.util.Optional;
+import mate.academy.dao.MovieDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            movie.setId((Long) session.save(movie));
+            transaction.commit();
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save movie - " + movie + " to database", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(Movie.class, id));
+        }
+    }
+}
