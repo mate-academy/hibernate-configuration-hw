bb6cc650f559705307443604a95219e405c83177
dant0n <dmibuc@gmail.com>
2021-05-22 18:49:42 +0300

    Implement suggested change.

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -13,11 +13,10 @@ import org.hibernate.Transaction;
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         Session session = null;
         Transaction transaction = null;
         try {
-            session = sessionFactory.openSession();
+            session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
             session.save(movie);
             transaction.commit();
