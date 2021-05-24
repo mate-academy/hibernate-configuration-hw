db93cf9c337313077b27196c19858a74851e3fa4
Dalai <semenov.dalay@gmail.com>
2021-05-24 15:28:18 +0300

    fixed solution

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -4,13 +4,16 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static final SessionFactory instance = initSessionFactory();
+    private static final SessionFactory sessionFactory = initSessionFactory();
 
-    public static SessionFactory initSessionFactory() {
+    private HibernateUtil() {
+    }
+
+    private static SessionFactory initSessionFactory() {
         return new Configuration().configure().buildSessionFactory();
     }
 
     public static SessionFactory getSessionFactory() {
-        return instance;
+        return sessionFactory;
     }
 }
