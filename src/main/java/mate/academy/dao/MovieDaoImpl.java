fdd8d7b2114e0f08e37c6de4c08bac04aefa688e
safordog <safordog@gmail.com>
2021-05-25 05:12:06 +0300

    added check

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -27,7 +27,9 @@ public class MovieDaoImpl implements MovieDao {
             }
             throw new DataProcessingException("Can't save movie: " + movie + " to DB.", e);
         } finally {
-            session.close();
+            if (session != null) {
+                session.close();
+            }
         }
     }
 
