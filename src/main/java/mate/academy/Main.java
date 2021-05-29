6c58c56073a3799c49607721b536f0f2948504d4
Никита Фоминых <nikitafominyh@MacBook-Pro-Nikita.local>
2021-05-29 21:58:17 +0300

    accepted task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,27 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+
+        Movie lastIsland = new Movie();
+        Movie gameOfThrones = new Movie();
+        lastIsland.setTitle("Last Island");
+        lastIsland.setDescription("That film about survival ...");
+        gameOfThrones.setDescription("Too many blood and dragon's");
+        gameOfThrones.setTitle("Game Of Thrones");
+
+        System.out.println(movieService.add(lastIsland));
+        System.out.println(movieService.add(gameOfThrones));
+
+        System.out.println(movieService.get(1L));
+        System.out.println(movieService.get(2L));
     }
 }
