6bda526775dd680fae554e900ee857dfa7985b5c
SerKisJD <sergej.kislyakov@gmail.com>
2021-05-30 22:15:58 +0300

    1 added solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,33 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    public static final Injector injector = Injector.getInstance("mate.academy");
+    public static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie solaris = new Movie();
+        solaris.setTitle("Solaris");
+        solaris.setDescription("Science-documental");
+
+        Movie saw = new Movie();
+        saw.setTitle("Saw");
+        saw.setDescription("Scary");
+
+        Movie mirrors = new Movie();
+        mirrors.setTitle("Mirrors");
+        mirrors.setDescription("Scary");
+
+        movieService.add(solaris);
+        movieService.add(saw);
+        movieService.add(mirrors);
+
+        System.out.println(movieService.get(1L));
+        System.out.println(movieService.get(2L));
+        System.out.println(movieService.get(3L));
     }
 }
---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,53 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.exceptions.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+            session.close();
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save movie: " + movie + " to DB.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            Movie movie = session.get(Movie.class, id);
+            return Optional.ofNullable(movie);
+        } catch (Exception e) {
+            throw new DataProcessingException("No such movie with id: " + id, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+    }
+}
