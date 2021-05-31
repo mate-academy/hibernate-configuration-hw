8f644775d6a6429a7afb6fcb9e3c17334b09eb8f
oleksandr-shulha <shulgalex@gmail.com>
2021-05-31 13:29:29 +0300

    First edition

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie shadows = new Movie();
+        shadows.setTitle("Shadows of Forgotten Ancestors");
+        shadows.setDescription("OST - You Have Already Gone to the Other World");
+        movieService.add(shadows);
+        System.out.println(shadows);
+        System.out.println(movieService.get(shadows.getId()));
     }
 }
