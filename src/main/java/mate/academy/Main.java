fdf9b732769a9bd3c4706c80226d8a84bd8abc55
Gord-Nik <75978452+Gord-Nik@users.noreply.github.com>
2021-05-31 14:59:57 +0300

    new program

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate");
+    private static final MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Forsage 7");
+        movie.setDescription("Olala");
+        movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
     }
 }
