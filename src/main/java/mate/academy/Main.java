93c2f2ec91396601fafde5944b2ec68abdb51bd9
anufriiev <dimaanufriev1012@gmail.com>
2021-05-31 19:00:43 +0300

    Solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,24 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static MovieService movieService
+            = (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        MovieDao movieDao = new MovieDaoImpl();
+        Movie movie = new Movie();
+        movie.setTitle("Film");
+        movie.setDescription("Film description");
+        movieDao.add(movie);
+
+        Movie movieFromDb = movieDao.get(1L);
+        System.out.println(movieFromDb);
     }
 }
---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,21 @@
+package mate.academy.service;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+    private MovieDao movieDao = new MovieDaoImpl();
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id);
+    }
+}
