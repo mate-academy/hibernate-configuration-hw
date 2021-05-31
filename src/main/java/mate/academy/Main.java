a03f8d7103009ecbb4930c5342bdb6408473d65b
Darina <to.darina.to>
2021-05-31 17:07:55 +0300

    all done all done

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,17 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector inject = Injector.getInstance("mate.academy");
+    private static final MovieService movieService = (MovieService)
+            inject.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie("Frozen 2", "broke your hart");
+        movieService.add(movie);
+        movieService.get(movie.getId());
     }
 }
