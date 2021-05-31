19a55aca58ef891a47a73792ce85fe9f59f8e2aa
the5tranger <mishaantonyuk00@gmail.com>
2021-05-31 15:38:14 +0300

    Innit commit (v1.0)

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService = (MovieService) injector
+            .getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie("BEST", "The bets of the best movie");
+
+        System.out.println("__________________________________________________________________");
+        movieService.add(movie);
+        Movie movieFromDB = movieService.get(1L);
+        System.out.println("__________________________________________________________________");
+        System.out.println(movieFromDB);
+        System.out.println("__________________________________________________________________");
     }
 }
