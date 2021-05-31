d71a7bda6e9823af1c1017af4a5356420cb6f89a
Cyouffy <cyouffy@gmail.com>
2021-05-31 17:36:31 +0300

    added solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,31 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    public static final Injector injector = Injector.getInstance("mate.academy");
+    public static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie matrix = new Movie();
+        matrix.setTitle("Matrix");
+        matrix.setDescription("Have you ever had a dream that you were so sure was real?");
+
+        Movie inception = new Movie();
+        inception.setTitle("Inception");
+        inception.setDescription("A thief who steals corporate secrets "
+                + "through the use of dream-sharing technology "
+                + "is given the inverse task of planting an idea into the mind of a C.E.O.");
+
+        movieService.add(matrix);
+        movieService.add(inception);
+        System.out.println(matrix);
+        System.out.println(inception);
+
+        System.out.println(movieService.get(matrix.getId()));
+        System.out.println(movieService.get(inception.getId()));
     }
 }
