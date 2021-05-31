901f45eda2af95a81d4f78faed7f61d54e45c5e8
ilia <bulkaus@gmail.com>
2021-05-31 19:56:54 +0300

    done task for review

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,30 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie inception = new Movie();
+        inception.setTitle("Inception");
+        inception.setDescription("Another side of life");
+
+        Movie johnWick = new Movie();
+        johnWick.setTitle("John Wick");
+        johnWick.setDescription("Everything has a price");
+
+        movieService.add(inception);
+        movieService.add(johnWick);
+
+        movieService.add(inception);
+        movieService.add(johnWick);
+
+        System.out.println(movieService.get(inception.getId()));
+        System.out.println(movieService.get(johnWick.getId()));
     }
 }
