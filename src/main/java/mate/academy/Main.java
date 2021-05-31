481ef52dee3f7803a372e428a26a5f2fd0e6035b
Alina <vasylyshyn.alina@gmail.com>
2021-05-31 20:15:46 +0300

    added solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static Injector injector = Injector.getInstance("mate");
+    private static MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie terminator1 = new Movie();
+        Movie avatar = new Movie();
+        terminator1.setTitle("Terminator 5");
+        terminator1.setDescription("Awesome movie");
+        avatar.setTitle("Avatar");
+        avatar.setDescription("Sci-fi movie");
+        movieService.add(terminator1);
+        movieService.add(avatar);
+        movieService.get(terminator1.getId());
+        movieService.get(avatar.getId());
     }
 }
---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,50 @@
+package mate.academy.dao;
+
+import mate.academy.Exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import java.util.Optional;
+
+@Dao
+public class MovieDaoImpl implements MovieDao{
+    @Override
+    public Movie add(Movie movie) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save product to DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try (Session session = sessionFactory.openSession()) {
+            Movie movie = session.get(Movie.class, id);
+            if(movie != null) {
+                return Optional.of(movie);
+            }
+            return Optional.empty();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get movie from DB", e);
+        }
+    }
+}
