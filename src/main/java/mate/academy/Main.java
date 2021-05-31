2df38fe7307254dd03fc601fd8cb50f446ededaf
Halina Kharlanava <galina.harlanova1@gmail.com>
2021-05-31 17:25:48 +0200

    Created all necessary files and classes according to README.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector =
+            Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        Movie joker = new Movie();
+        joker.setTitle("Joker");
+        joker.setDescription("Great movie");
+
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+        System.out.println(movieService.add(joker));
+        System.out.println(movieService.get(1L));
     }
 }
