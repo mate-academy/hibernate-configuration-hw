f38992d68e20d075e5ec52cda8294b9b9ff2915e
Kostiantyn <konkos2505@gmail.com>
2021-05-31 21:48:22 +0300

    resolved some mistakes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -9,9 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
-        Movie movie = new Movie();
-        movie.setTitle("Terminator 5");
-        movie.setDescription("Awesome movie");
+        Movie movie = new Movie("Terminator", "Awesome movie");
         movieService.add(movie);
         System.out.println(movieService.get(movie.getId()));
     }
--- src/main/java/mate/academy/dao/MovieDao.java
+++ src/main/java/mate/academy/dao/MovieDao.java
@@ -1,11 +1,10 @@
 package mate.academy.dao;
 
 import java.util.Optional;
-import mate.academy.lib.exception.DataProcessingException;
 import mate.academy.model.Movie;
 
 public interface MovieDao {
-    Movie save(Movie movie) throws DataProcessingException;
+    Movie save(Movie movie);
 
-    Optional<Movie> get(Long id) throws DataProcessingException;
+    Optional<Movie> get(Long id);
 }
