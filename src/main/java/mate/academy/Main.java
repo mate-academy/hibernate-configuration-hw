d7d4228bc1c1a2e36c6c4f205f7eefe3fd3774e5
Darina <to.darina.to>
2021-05-31 17:18:30 +0300

    student review1 done

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -12,6 +12,6 @@ public class Main {
     public static void main(String[] args) {
         Movie movie = new Movie("Frozen 2", "broke your hart");
         movieService.add(movie);
-        movieService.get(movie.getId());
+        System.out.println(movieService.get(movie.getId()));
     }
 }
--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -12,7 +12,6 @@ public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
     private String description;
 
