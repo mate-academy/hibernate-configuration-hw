eb8dd06857ed40d6b68547b770540579b363bbbc
Yevhenii Denysenko <iedenysenko@gmail.com>
2021-05-31 10:57:17 +0300

    resolve new some mistakes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,16 +6,17 @@ import mate.academy.service.MovieService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
     
     public static void main(String[] args) {
-        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
-        
         Movie matrix = new Movie();
         matrix.setTitle("Matrix");
         matrix.setDescription(
                 "After years of searching, Morpheus and Trinity finally find information about a "
                         + "man that could possibly be The One they have been searching for who is"
                         + " capable of manipulating the Matrix universe.");
+        
         Movie terminator = new Movie();
         terminator.setTitle("Terminator");
         terminator.setDescription("No fate");
