5506bd9bd13f33012f9127a0dc68fadaeec0fd67
Iryna Ustymenko <i.ustimenko18@gmail.com>
2021-05-31 14:59:15 +0300

    created hibernate.cfg.xml file, HibernateUtil class, Movie model class, MovieDao interface and MovieDaoImpl clas

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,24 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService manufacturerService = (MovieService) injector
+                .getInstance(MovieService.class);
 
+        Movie movieIsaura = new Movie("Escrava Isaura", "Awesome film");
+        Movie movieClone = new Movie("O Clone", "Brazilian telenovela");
+
+        movieIsaura = manufacturerService.add(movieIsaura);
+        movieClone = manufacturerService.add(movieClone);
+        System.out.println(movieIsaura);
+        System.out.println(movieClone);
+
+        System.out.println(manufacturerService.get(movieIsaura.getId()));
+        System.out.println(manufacturerService.get(movieClone.getId()));
     }
 }
