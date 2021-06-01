6e41bbc233da567aa43482b7af06b3416b73d2f0
Bohdan Borsh <borshbohdan@gmail.com>
2021-06-01 15:46:41 +0300

    HW done.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,27 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
     public static void main(String[] args) {
 
+        Movie titanicMovie = new Movie();
+        titanicMovie.setTitle("Titanic");
+        titanicMovie.setDescription("Unsinkable ship sunk");
+        movieService.add(titanicMovie);
+
+        Movie terminator2Movie = new Movie();
+        terminator2Movie.setTitle("Terminator 2");
+        terminator2Movie.setDescription("Now with a good cyborg!");
+        movieService.add(terminator2Movie);
+
+        System.out.println(movieService.get(titanicMovie.getId()));
+        System.out.println(movieService.get(terminator2Movie.getId()));
     }
 }
