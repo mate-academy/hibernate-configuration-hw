52377244614811780e9c7a6d777c9c9144320794
mbelm <mbelmasg@gmail.com>
2021-07-09 18:17:23 +0300

    start

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,6 +1,13 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final MovieService movieService = (MovieService) injector
+            .getInstance(MovieService.class);
+
     public static void main(String[] args) {
 
     }
