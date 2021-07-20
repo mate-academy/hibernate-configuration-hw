b2ae188e65f5111ea60d44af0de19bc30097c8ec
Viacheslav Cheban <vcheb@eg.dk>
2021-07-20 23:36:12 +0300

    add HibernateUtil

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -1,7 +1,22 @@
 package mate.academy.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class HibernateUtil {
 
     private HibernateUtil() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("mate-academy");
+        EntityManager entityManager = emf.createEntityManager();
+        // Get the Hibernate Session from the EntityManager in JPA
+        Session session = entityManager.unwrap(org.hibernate.Session.class);
+        return session.getSessionFactory();
     }
 }
