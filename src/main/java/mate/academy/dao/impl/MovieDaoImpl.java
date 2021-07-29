e34b78a3d29eaec9f364b32fb079fc9be69e6bd2
Tamara-Paskalova <forptv@gmail.com>
2021-07-29 19:53:41 +0300

    created Movie model class, DataProcessingException, DaoMovie layer, ServiceMovie layer, added dependencies to pom.xnl

---
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,38 @@
+package mate.academy.dao.impl;
+
+import java.util.Optional;
+import mate.academy.dao.MovieDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+    @Override
+    public Movie add(Movie movie) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(movie);
+        transaction.commit();
+        session.close();
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        Session session = sessionFactory.openSession();
+        Movie movie = session.get(Movie.class, id);
+        session.close();
+        try {
+            return Optional.of(movie);
+        } catch (Exception e) {
+            throw new DataProcessingException("Could not get Movie by Id: " + id, e);
+        }
+    }
+}
