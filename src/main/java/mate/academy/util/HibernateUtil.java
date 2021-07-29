9991cfb0e5f7f0b79e55ab061f911255dd729c69
Borys Skvortsov <Dhakmar@gmail.com>
2021-07-29 15:39:04 +0300

    Created HibernateUtil class

---
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -0,0 +1,14 @@
+package mate.academy.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory SESSION_FACTORY = new Configuration()
+            .configure()
+            .buildSessionFactory();
+
+    public static SessionFactory getSessionFactory() {
+        return SESSION_FACTORY;
+    }
+}
