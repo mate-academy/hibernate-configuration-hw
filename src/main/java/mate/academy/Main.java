d97261db99a4da03af1ac623ec1d29260ada7370
nikdagaev <nikdagaev@gmail.com>
2021-07-29 18:34:14 +0300

    Configured Hibernate. Tested in Main method.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService = (MovieService) injector
+            .getInstance(MovieService.class);
+    public static void main(String[] args) {
+        Movie avengers = new Movie();
+        avengers.setTitle("Avengers");
+        avengers.setDescription("Another Marvel movie");
+        Movie ironMan = new Movie();
+        ironMan.setTitle("Iron Man");
+        ironMan.setDescription("And another Marvel movie");
+        movieService.add(avengers);
+        movieService.add(ironMan);
+        movieService.get(1L);
+        movieService.get(2L);
     }
 }
