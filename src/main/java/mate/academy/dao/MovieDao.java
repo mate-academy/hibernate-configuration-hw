c395e915d8dafab7d509e053d002a5b2414ee12f
Yura Lug <LUV97@ukr.net>
2021-07-30 16:33:43 +0300

    Fix checkstyle and pom

--- src/main/java/mate/academy/dao/MovieDao.java
+++ src/main/java/mate/academy/dao/MovieDao.java
@@ -1,8 +1,7 @@
-package mate.academy.Dao;
-
-import mate.academy.model.Movie;
+package mate.academy.dao;
 
 import java.util.Optional;
+import mate.academy.model.Movie;
 
 public interface MovieDao {
     Movie add(Movie movie);
--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -4,11 +4,16 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static SessionFactory insnance = getSessionFactory();
+    private static SessionFactory instance = getSessionFactory();
+
+    private HibernateUtil() {
+
+    }
 
     private static SessionFactory initSessionFactory() {
         return new Configuration().configure().buildSessionFactory();
     }
+
     public static SessionFactory getSessionFactory() {
         return initSessionFactory();
     }
