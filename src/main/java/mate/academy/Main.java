ed90c58e0f36ebe0a5009e5566a8d23957f7cdf4
aaananas <anastasiia.len11@gmail.com>
2021-07-30 08:51:53 +0200

    added Main, and tested MovieDao methods

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie matrixOne = new Movie();
+        matrixOne.setTitle("The Matrix 1");
+        movieService.add(matrixOne);
+        System.out.println(matrixOne);
+        System.out.println(movieService.get(matrixOne.getId()));
+
+        Movie matrixTwo = new Movie();
+        matrixTwo.setDescription("Another part of The Matrix");
+        matrixTwo.setTitle("The Matrix 2");
+        movieService.add(matrixTwo);
+        System.out.println(matrixTwo);
+        System.out.println(movieService.get(matrixTwo.getId()));
     }
 }
