3f22c134f2291dd97554649b823f63545fa69d02
Nikitozin <nikitozin91@gmail.com>
2021-07-30 12:07:49 +0300

    Add_Some_Tests_In_Main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Taxi");
+        movie.setDescription("There is no more a gambler than a driver!");
+        movieService.add(movie);
+        System.out.println(movieService.get(1L).getTitle());
     }
 }
