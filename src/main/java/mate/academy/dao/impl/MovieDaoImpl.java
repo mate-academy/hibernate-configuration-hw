63c89b11eb38840bf339f6ec15e918587cd9a86d
Borys Skvortsov <Dhakmar@gmail.com>
2021-07-30 12:48:47 +0300

    Fixed some mistakes;

--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -7,19 +7,17 @@ import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
-    private static final SessionFactory SESSION_FACTORY = HibernateUtil.getSessionFactory();
 
     @Override
     public Movie add(Movie movie) {
         Session session = null;
         Transaction transaction = null;
         try {
-            session = SESSION_FACTORY.openSession();
+            session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
             session.save(movie);
             transaction.commit();
@@ -38,17 +36,10 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public Optional<Movie> get(Long id) {
-        Session session = null;
-        try {
-            session = SESSION_FACTORY.openSession();
-            Movie movie = session.get(Movie.class, id);
-            return Optional.ofNullable(movie);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(Movie.class, id));
         } catch (Exception e) {
             throw new DataProcessingException("Can`t get movie by id: " + id, e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
     }
 }
--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -4,9 +4,16 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static final SessionFactory SESSION_FACTORY = new Configuration()
-            .configure()
-            .buildSessionFactory();
+    private static final SessionFactory SESSION_FACTORY = initSessionFactory();
+
+    private HibernateUtil() {
+    }
+
+    private static SessionFactory initSessionFactory() {
+        return new Configuration()
+                .configure()
+                .buildSessionFactory();
+    }
 
     public static SessionFactory getSessionFactory() {
         return SESSION_FACTORY;
