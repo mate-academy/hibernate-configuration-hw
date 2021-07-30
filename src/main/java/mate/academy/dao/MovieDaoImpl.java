70c586f6b1372759f36c5f1d9be7878d9128fcbc
EugeneSinica <eugenesinica@gmail.com>
2021-07-30 13:48:57 +0300

    ready project structure

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,17 @@
+package mate.academy.dao;
+
+import mate.academy.model.Movie;
+
+import java.util.Optional;
+
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        return null;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,20 @@
+package mate.academy.service;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Inject;
+import mate.academy.model.Movie;
+
+public class MovieServiceImpl implements MovieService {
+    @Inject
+    MovieDao movieDao;
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id).get();
+    }
+}
