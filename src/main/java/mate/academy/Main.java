5a1c65edf78b3b5c56a409977752d75d23becad7
Benito <zara.benito17@gmail.com>
2021-07-30 11:34:14 +0300

    added solution;

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("The Gentlemen");
+        movie.setDescription("An American expat tries to sell off his highly profitable"
+                + " marijuana empire in London, triggering plots, schemes, bribery "
+                + "and blackmail in an attempt to steal his domain out from under him.");
+        movieService.add(movie);
+
+        System.out.println(movieService.get(1L).toString());
     }
 }
