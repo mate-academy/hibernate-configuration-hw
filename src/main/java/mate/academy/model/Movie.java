f81e0b2e828b0bd7999b47045b2a495923af76dc
Daniel Kuruch <kuruch1004@gmail.com>
2021-07-30 13:46:31 +0300

    implemented dao, service, entity hibernate class movie

---
+++ src/main/java/mate/academy/model/Movie.java
@@ -0,0 +1,60 @@
+package mate.academy.model;
+
+import java.util.StringJoiner;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String title;
+    String description;
+
+    public Movie() {
+    }
+
+    public Movie(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Movie.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("title='" + title + "'")
+                .add("description='" + description + "'")
+                .toString();
+    }
+}
