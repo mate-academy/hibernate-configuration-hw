7962a5f5e9e43263e19ccae8f570ae0fa3542578
vKutik <84785564+vKutik@users.noreply.github.com>
2021-07-30 16:22:14 +0300

    ADD LOGIC

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,16 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.impl.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movieOne = new Movie("Rick and Morty", "film");
+        movieService.add(movieOne);
+        System.out.println(movieService.get(1l));
 
     }
 }
