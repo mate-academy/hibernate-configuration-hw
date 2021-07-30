ebbbcd7a224532547c0a75b485f3f76a5351e050
Yura Lug <LUV97@ukr.net>
2021-07-30 15:31:21 +0300

    Ct=reate hibernate interface for 1 table

---
+++ src/main/java/mate/academy/Dao/MovieDao.java
@@ -0,0 +1,11 @@
+package mate.academy.Dao;
+
+import mate.academy.model.Movie;
+
+import java.util.Optional;
+
+public interface MovieDao {
+    Movie add(Movie movie);
+
+    Optional<Movie> get(Long id);
+}
--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
     public static void main(String[] args) {
+        Movie movie1 = new Movie();
+        movie1.setTitle("Dracula");
+        movie1.setDescription("It could be a start new world");
+        movieService.add(movie1);
+        System.out.println(movieService.get(movie1.getId()));
 
     }
 }
