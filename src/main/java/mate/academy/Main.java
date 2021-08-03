8931f96112e7d616a8d96eaaf11ba69379c65318
olekskov <oleksandr@kovalenko.cc>
2021-08-03 19:11:15 +0000

    Hibernate-configuration-hw solution
    Initial solution:
    - Checkstyle maven plugin added
    - Hibernate configured and util created
    - Movie model/dao/service implemented
    - Tested in PSVM

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,36 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    @Inject
+    private static MovieService movieService
+            = (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("The Last Samurai", "drama"));
+        movies.add(new Movie("Naked Gun", "comedy"));
+        movies.add(new Movie("Saw", "horror"));
+        movies.add(new Movie("Zoopolis", "animation"));
+
+        movies.forEach(movieService::add);
+
+        for (Long i = 1L; i <= movies.size(); i++) {
+            System.out.println(movieService.get(i));
+        }
+
+        try {
+            movieService.get((long) (movies.size() + 1));
+        } catch (NoSuchElementException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
