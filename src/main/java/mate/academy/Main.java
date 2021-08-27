6c75bb985f3bb40b99e8b866d3c751094e748e29
Tetiana Chupika <mtv1301@gmail.com>
2021-08-27 15:00:42 +0300

    added solution to hibernate home work

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieDao movieDao = (MovieDao) injector.getInstance(MovieDao.class);
+        Movie movie = new Movie();
+        movie.setTitle("Terminator 5");
+        movie.setDescription("Film about Terminator");
+
+        movieDao.add(movie);
+        System.out.println(movieDao.get(1L));
     }
 }
