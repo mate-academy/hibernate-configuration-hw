ca40b22f5fbc06b9db1ce486a4ed7c37cede631a
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-09-21 17:34:07 +0300

    Hibernate configuration completed

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,27 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
+        final MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movieDune = new Movie();
+        movieDune.setTitle("Dune");
+        movieDune.setDescription("Epic science fiction film ");
+        movieService.add(movieDune);
+
+        Movie moviePawPatrol = new Movie();
+        moviePawPatrol.setTitle("Paw Patrol");
+        moviePawPatrol.setDescription("Movie for children");
+        movieService.add(moviePawPatrol);
+
+        Movie movieFromDB = movieService.get(1L);
+        System.out.println(movieFromDB);
 
     }
 }
