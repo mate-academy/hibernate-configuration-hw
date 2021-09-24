5bc2a98b67ff25b93200f98ca742bdda1e37b9c3
mishamolnar <michaellmuller423@gmail.com>
2021-09-24 15:10:48 +0300

    service and dao added

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,21 +1,20 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
-import mate.academy.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import mate.academy.service.MovieService;
 
 public class Main {
+    private static final Injector injector =
+            Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        SessionFactory sessionFactory =
-                HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+        MovieService movieService = (MovieService)
+                injector.getInstance(MovieService.class);
         Movie movie = new Movie();
         movie.setTitle("Fast and Furious");
-        session.save(movie);
-        transaction.commit();
-        session.close();
+        movie.setDescription("Best movie ever");
+        movieService.add(movie);
+        System.out.println(movieService.get(1L));
     }
 }
--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -1,15 +1,22 @@
 package mate.academy.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "movies")
 public class Movie {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
+
+    public Movie() {
+    }
 
     public Long getId() {
         return id;
