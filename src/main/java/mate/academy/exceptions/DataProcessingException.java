971b5d395d4d921b95ae2e8832404fd345176485
Sinelife <ya.serzhan@gmail.com>
2021-09-24 12:41:50 +0300

    changed DataProcessingException to Exception in catch block

--- src/main/java/mate/academy/exceptions/DataProcessingException.java
+++ src/main/java/mate/academy/exceptions/DataProcessingException.java
@@ -1,7 +1,7 @@
 package mate.academy.exceptions;
 
 public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String message, RuntimeException e) {
+    public DataProcessingException(String message, Exception e) {
         super(message, e);
     }
 }
