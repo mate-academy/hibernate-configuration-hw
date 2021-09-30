bd52a1d47a1e3b94a39df995b8eec56a4301ad17
Kostia Dementiev <konstde00@gmail.com>
2021-09-30 15:24:14 +0300

    fixed getMovie method

--- src/main/java/mate/academy/exception/DataProcessingException.java
+++ src/main/java/mate/academy/exception/DataProcessingException.java
@@ -4,4 +4,7 @@ public class DataProcessingException extends RuntimeException {
     public DataProcessingException(String message, Exception e) {
         super(message, e);
     }
+
+    public DataProcessingException(String message) {
+    }
 }
