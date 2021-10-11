99cb6de125caa74d5039669277f7dec251a064f8
Serhii Orlov <orlov30@gmail.com>
2021-10-11 17:17:05 +0300

    hibernate intro practise. create entity Movie.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.sevice.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService
+            = (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Lord of Ring: 1");
+        System.out.println(movieService.add(movie));
+        System.out.println(movieService.get(movie.getId()));
     }
 }
