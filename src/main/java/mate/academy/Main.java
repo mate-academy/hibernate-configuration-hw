6e9d02d071ed792a8b3696b5b72e7822e272bda0
Sasha Hordenin <sg743148@gmail.com>
2021-10-21 17:44:14 +0300

    realized hibernate-configuration-hw

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector
+                .getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Lord of the Rings");
+        movie.setDescription("The Lord of the Rings is a series of three epic fantasy adventure "
+                + "films directed by Peter Jackson,"
+                + " based on the novel written by J. R. R. Tolkien.");
+        Movie addedMovie = movieService.add(movie);
+        System.out.println(addedMovie);
+        Movie gotMovie = movieService.get(1L);
+        System.out.println(gotMovie);
     }
 }
