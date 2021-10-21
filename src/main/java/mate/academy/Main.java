d31c7e584a7bd2e8c835fd45c03e01871561cb82
Andrii Demchenko <avd998ne@gmail.com>
2021-10-21 17:42:49 +0300

    added hibernate configuration solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,21 +1,19 @@
 package mate.academy;
 
+
+import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
-import mate.academy.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import mate.academy.service.MovieService;
 
 public class Main {
+    public static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         Movie movie = new Movie();
         movie.setTitle("Scarface");
-        movie.setDescription("Something about scarface");
-        session.save(movie);
-        transaction.commit();
-        session.close();
+        movie.setDescription("Scarface description");
+        movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
     }
 }
