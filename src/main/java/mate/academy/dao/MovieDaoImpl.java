a814af45b979424aabe2c668111a96ddf4c7f70b
SergiiLonskyi <sergii.lonskyi@gmail.com>
2021-10-21 17:36:35 +0300

    Implemented hibernate configuration task

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,46 @@
+package mate.academy.dao;
+
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import java.util.Optional;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+        } catch (DataProcessingException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Couldn't add movie: " + movie + " to DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+           Movie movie = session.get(Movie.class, id);
+           return Optional.ofNullable(movie);
+        } catch (DataProcessingException e) {
+            throw new DataProcessingException("Couldn't find movie with id " + id + " in DB", e);
+        }
+    }
+}
