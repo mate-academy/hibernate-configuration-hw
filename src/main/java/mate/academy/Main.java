aaf6346cf129e2088dd4fbf9d839097e0afcf58c
Roman Dandali <Dandali2018@gmail.com>
2021-10-22 13:10:07 +0300

    move injector field

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -5,8 +5,9 @@ import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        Injector injector = Injector.getInstance("mate.academy");
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         Movie movie = new Movie();
         movie.setTitle("Forrest Gump");
