883c17d33e86df7d12524a1e1a571c3db2dc5dc5
Vitalii Mykh <vitaliymykh@gmail.com>
2021-10-22 14:08:58 +0300

    Hibernate configured, access to DB set

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static MovieService movieService = (MovieService) injector
+            .getInstance(MovieService.class);
+
     public static void main(String[] args) {
 
+        MovieDao movieDao = new MovieDaoImpl();
+        Movie movie = new Movie();
+        movie.setId(10L);
+        movie.setTitle("Terminator 5");
+        movie.setDescription("awesome movie");
+
+        Movie addToDB = movieService.add(movie);
+        Movie movieFromDB = movieService.get(1L);
+        System.out.println(movieFromDB);
     }
 }
---
+++ src/main/java/mate/academy/exception/DataProcessingException.java
@@ -0,0 +1,14 @@
+package mate.academy.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message) {
+        super(message);
+    }
+
+    public DataProcessingException() {
+    }
+
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
