1f47ba5c469f8d415c97e0cf2ea35a778a7819ce
Dmytro Heleta <dmytroheleta@gmail.com>
2021-10-22 13:13:46 +0300

    implemented MovieService and MovieDao

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService
+            = (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie homeAlone = new Movie();
+        homeAlone.setTitle("Home alone");
+        Movie fargo = new Movie();
+        homeAlone.setTitle("Fargo");
+        System.out.println(movieService.add(homeAlone));
+        System.out.println(movieService.get(homeAlone.getId()));
+        System.out.println(movieService.add(fargo));
+        System.out.println(movieService.get(fargo.getId()));
     }
 }
