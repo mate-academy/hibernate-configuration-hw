ec277e041f6c3d55434260ca875bf1c464214136
Roman Dandali <Dandali2018@gmail.com>
2021-10-22 12:24:18 +0300

    Completed all items from README.md

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,17 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
-
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Forrest Gump");
+        movie.setDescription("Run Forrest!");
+        movieService.add(movie);
+        movieService.get(1L);
     }
 }
