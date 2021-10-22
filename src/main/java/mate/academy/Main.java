c6a08f2700331d6247c5ee2a6a2317644ec9333c
Vitalii Mykh <vitaliymykh@gmail.com>
2021-10-22 15:05:42 +0300

    deleted redundant dao object from main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,5 @@
 package mate.academy;
 
-import mate.academy.dao.MovieDao;
-import mate.academy.dao.MovieDaoImpl;
 import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
@@ -12,8 +10,6 @@ public class Main {
             .getInstance(MovieService.class);
 
     public static void main(String[] args) {
-
-        MovieDao movieDao = new MovieDaoImpl();
         Movie movie = new Movie();
         movie.setTitle("Terminator 5");
         movie.setDescription("awesome movie");
