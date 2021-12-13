5c4ca7319d20634a1131c821415d8fe0d229e642
Anton Volskyi <antonvolskyi1234@gmail.com>
2021-12-13 19:21:14 +0200

    hibernate add and work

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
+import mate.academy.util.HibernateUtil;
+import model.Movie;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 public class Main {
     public static void main(String[] args) {
-
+        Movie movie = new Movie("Goverment", "Big Bos and You");
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(movie);
+        transaction.commit();
+        session.close();
     }
 }
