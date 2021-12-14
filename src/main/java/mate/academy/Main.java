af1f335fe3f909b0a0d24d3fd0400f65859ec73d
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2021-12-15 00:42:09 +0200

    HibernateUtil, MovieDao and MovieService were implemeted. Checkstyle maven plugin was added

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+
+        Movie toyStory = new Movie();
+        toyStory.setTitle("Toy Story 1");
+        toyStory.setDescription("American computer-animated comedy film "
+                + "produced by Pixar Animation Studios");
+        movieService.add(toyStory);
+        Movie movieFromDB = movieService.get(toyStory.getId());
+        System.out.println(movieFromDB);
     }
 }
