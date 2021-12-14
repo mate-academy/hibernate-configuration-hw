cfc2b6f0724d453baf2fe98754814a05c18fcf73
Ihor Khominich <ikhominitch@gmail.com>
2021-12-15 00:12:27 +0200

    solution done

---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,25 @@
+package mate.academy.service;
+
+import java.util.NoSuchElementException;
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+    @Inject
+    private MovieDao movieDao;
+
+    @Override
+    public Movie save(Movie movie) {
+        return movieDao.save(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id)
+                .orElseThrow(() -> new NoSuchElementException("There is no movie with id "
+                        + id + "in DB"));
+    }
+}
