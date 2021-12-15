2fa516a5599d776da83d72dbb01f0809c5b556f7
Zachar Chorny <ufumoveldar@gmail.com>
2021-12-15 18:50:19 +0200

    Implemented the ability to communicate with the database using hibernate

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate");
+        MovieService movieService = (MovieService) injector
+                .getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("Dark Knight");
+        movie.setDescription("New movie");
+
+        movieService.add(movie);
+
+        System.out.println(movieService.get(1L));
     }
 }
