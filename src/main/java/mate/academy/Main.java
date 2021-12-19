9a2d0ae14a63762423cf674282b3dc6637423594
SarumanIsAlive <90073141+SarumanIsAlive@users.noreply.github.com>
2021-12-19 16:10:24 +0200

    Implemented all the steps from readme.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie("Lord Of The Rings", "Best of the Best Thing number ONE!");
+        movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
     }
 }
