0bda7ebb192f3ab1fd1b9b0b0b5364d98b98804f
Anatolii Huryk <tolik.mario96@gmail.com>
2021-12-19 23:07:10 +0200

    fix

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -8,13 +8,12 @@ public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
 
     public static void main(String[] args) {
-        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
-        Movie scaryMovie = new Movie();
-        scaryMovie.setTitle("Gone Girl");
-        scaryMovie.setDescription("With his wife's disappearance having become the focus of "
-                + "an intense media circus, a man s the spotlight turned on him when "
-                + "it's suspected that he may not be innocent.");
-        movieService.add(scaryMovie);
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Gone Girl");
+        movie.setDescription("Crazy woman");
+        movieService.add(movie);
         System.out.println(movieService.get(1L));
     }
 }
