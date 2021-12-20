cb1ca6e42370245371aab794c19c0dfcc78c5055
Dany Nesterov <nesterov1200997@gmail.com>
2021-12-20 22:10:34 +0200

    hibernate-awesome!!!

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie matrix = new Movie();
+        matrix.setTitle("Matrix");
+        matrix.setDescription("4th part is coming!!!");
+        movieService.add(matrix);
+
+        Movie matrixByID = movieService.get(matrix.getId());
+        System.out.println(matrixByID);
     }
 }
