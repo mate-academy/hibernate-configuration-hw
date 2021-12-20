6c975297a679c28099ca4fa1427818ee4260200a
Dany Nesterov <nesterov1200997@gmail.com>
2021-12-20 22:30:21 +0200

    hibernate-hw v1.1

--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -38,13 +38,19 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public Optional<Movie> get(Long id) {
+        Session session = null;
         try {
             SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-            Session session = sessionFactory.openSession();
+            session = sessionFactory.openSession();
             Movie movie = session.get(Movie.class, id);
-            return Optional.of(movie);
+            return Optional.ofNullable(movie);
         } catch (Exception e) {
             throw new DataProcessingException("Couldn't get a Movie by id " + id, e);
+        }
+        finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 }
