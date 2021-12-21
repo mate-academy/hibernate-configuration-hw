05eb1d2d84eb84bc719adc5400cdfaa3b9a42ef1
Mykola Voronov <www.kolyavoronov@gmail.com>
2021-12-21 08:33:12 +0200

    Hibernate was configured. Movie model with dao and service layers was created

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector
+                .getInstance(MovieService.class);
+        Movie harry = new Movie();
+        harry.setTitle("Harry Potter and the Philosopher's stone");
+        harry.setDescription("Fantasy, Adventures, Action");
+        movieService.add(harry);
+        System.out.println(movieService.get(harry.getId()));
     }
 }
