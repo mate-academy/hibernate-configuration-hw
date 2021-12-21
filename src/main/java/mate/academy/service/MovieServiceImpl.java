2100717220b0fbd27515c88829d2a776bd4732c2
Andriy-dz <andriydzdz@gmail.com>
2021-12-21 13:56:41 +0200

    checkstyle

--- src/main/java/mate/academy/service/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -1,7 +1,6 @@
 package mate.academy.service;
 
 import mate.academy.dao.MovieDao;
-import mate.academy.exeption.DataProcessingException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.Movie;
