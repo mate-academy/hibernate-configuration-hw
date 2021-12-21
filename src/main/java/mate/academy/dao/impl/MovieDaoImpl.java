35c10f074837f10e481270926a6d71fa50fb321c
Alexsis Moskovchuk <alekssiuss@gmail.com>
2021-12-21 11:04:32 +0200

    cleanup

--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -37,9 +37,9 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public Optional<Movie> get(Long id) {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return Optional.ofNullable(session.get(Movie.class, id));
-    } catch (Exception e) {
+        } catch (Exception e) {
             throw new DataProcessingException("Can't get movie from DB " + id, e);
         }
     }
