872fb0ae602d68ac2fb3cb638a21e9ca3ad9d909
Stanislav <stassonich@icloud.com>
2022-02-22 01:13:38 +0200

    hibernate task

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,56 @@
+package mate.academy.dao;
+
+import mate.academy.exeption.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.Optional;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+        } catch (HibernateException hibernateException) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Couldn't save movie: "
+                    + movie, hibernateException);
+
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        Movie movie;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            movie = session.get(Movie.class, id);
+        } catch (HibernateException hibernateException) {
+            throw new DataProcessingException("Couldn't get movie by id: "
+                    + id, hibernateException);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return Optional.ofNullable(movie);
+    }
+}
