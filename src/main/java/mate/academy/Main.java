ec27735b78d9b483687918ef5addee42458e4f70
sviat <svyatoslav.grinik@gmail.com>
2022-02-22 19:55:26 +0200

    added tests to the main class

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,17 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        movieService.add(Movie.builder().title("Interstellar").description("My favorite movie")
+                .build());
+        System.out.println(movieService.get(1L));
     }
 }
