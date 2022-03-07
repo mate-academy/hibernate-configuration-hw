1b13dff4ae0ab240ec4ce1e66a2b7cd7b32cfc4a
Maksym Pokras <mxpokras@gmail.com>
2022-03-07 15:12:18 +0200

    DataProcessingException unnecessary constructor removed, MovieDaoImpl exception message modified

--- src/main/java/mate/academy/exception/DataProcessingException.java
+++ src/main/java/mate/academy/exception/DataProcessingException.java
@@ -2,10 +2,6 @@ package mate.academy.exception;
 
 public class DataProcessingException extends RuntimeException {
 
-    public DataProcessingException(String message) {
-        super(message);
-    }
-
     public DataProcessingException(String message, Throwable cause) {
         super(message, cause);
     }
