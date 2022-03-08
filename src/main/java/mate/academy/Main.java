f4142c05bb921bb4bf2a0601c046bc18382cceac
LinkClink <shekaqwe@gmail.com>
2022-03-08 10:45:20 +0200

    test in main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(createMovie());
+        Movie movie = movieService.get(1L);
+        System.out.println(movie);
+    }
+
+    private static Movie createMovie() {
+        Movie movie = new Movie();
+        movie.setDescription("American buddy drama film written and directed by Mike Binder");
+        movie.setTitle("Reign Over Me");
+        return movie;
     }
 }
