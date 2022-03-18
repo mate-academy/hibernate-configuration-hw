a7af0f8bcdd1edc08e9ba9b29c451f961dc335f4
djmolot <djmolot@gmail.com>
2022-03-18 08:58:13 +0100

    fixed errors in class Main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -5,20 +5,18 @@ import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
 
 public class Main {
-
     private static Injector injector = Injector.getInstance("mate.academy");
 
     public static void main(String[] args) {
-
         MovieService movieService =
                 (MovieService) injector.getInstance(MovieService.class);
 
         Movie movie = new Movie();
         movie.setTitle("Terminator 2");
         movie.setDescription("Awesome movie");
-        movieService.add(movie);
+        Movie returnedMovie = movieService.add(movie);
 
-        Movie movieFromDB = movieService.get(1L);
+        Movie movieFromDB = movieService.get(returnedMovie.getId());
         System.out.println("movieFromDB: " + movieFromDB);
 
     }
