5905a68e3572d20dcc1f73b82126dbfaa695c604
Andriy_Poluhovic <chimbar@ukr.net>
2022-03-18 18:03:48 +0200

    Create some movie

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,29 +1,26 @@
 package mate.academy.dao;
 
+import java.util.Optional;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-
-import java.util.Optional;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
 
     @Override
     public Movie add(Movie movie) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         Session session = null;
         Transaction transaction = null;
         try {
-            session = sessionFactory.openSession();
+            session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
             session.save(movie);
-            transaction.commit();;
+            transaction.commit();
         } catch (HibernateException ex) {
             if (transaction != null) {
                 transaction.rollback();
@@ -40,8 +37,7 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public Optional<Movie> get(Long id) {
-        try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-             Session session = sessionFactory.openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return Optional.ofNullable(session.get(Movie.class, id));
         } catch (HibernateException ex) {
             throw new DataProcessingException("Can't get movie from DB by id: "
