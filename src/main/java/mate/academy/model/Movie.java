f31f4a853f3bee0410442895981f96d5494785c3
djmolot <djmolot@gmail.com>
2022-03-19 14:47:22 +0100

    added annotations above the id field

--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -9,6 +9,8 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "movies")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
