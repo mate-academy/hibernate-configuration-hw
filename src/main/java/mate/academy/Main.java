cc967915accfae07f7c62e7fb45160ff9d025a5a
anyutamysko <a_misko@ukr.net>
2022-03-25 22:26:24 +0200

    config init

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,17 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Inject;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    public static Injector injector = Injector.getInstance("mate.academy");
+    public static void main(String[] args) {
+       MovieService  movieService = (MovieService) injector.getInstance(MovieService.class);
+       Movie movie = new Movie("The Godfather", "The aging patriarch of an organized crime dynasty " +
+               "in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.");
+       movie = movieService.add(movie);
+       System.out.println(movieService.get(movie.getId()));
     }
 }
