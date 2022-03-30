d0be1e820a118c3349ad9a8d69cf126cb5a4294a
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-03-30 15:32:53 +0300

    Tested in Main class

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    public static Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Lord of rings");
+        movie.setDescription("Awesome movie");
+        System.out.println("Adding movie:");
+        movie = movieService.add(movie);
+        System.out.println(movie);
+        System.out.println("Movie was added.");
+        System.out.println(".");
+        System.out.println(".");
+        System.out.println(".");
+        System.out.println("Get movie by id: " + movie.getId());
+        System.out.println(movieService.get(movie.getId()));
     }
 }
