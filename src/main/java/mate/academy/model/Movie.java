f69ec6674105a4a7afd21cdf8a99da6524688d44
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-03-30 14:35:46 +0300

    Movie model was implemented

--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -1,5 +1,41 @@
 package mate.academy.model;
 
-public class Movie {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String title;
+    String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
