7398594bab4c9de33095dc041e8178f694f96808
sltnsrh <lifenjoy8888@gmail.com>
2022-04-01 15:56:29 +0300

    hibernate configuration was implemented

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    public static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Superman 20");
+        movie.setDescription("Some description");
+        System.out.println("Movie " + movieService.add(movie)
+                + " was successfully added");
+        System.out.println("Get movie: " + movieService.get(movie.getId()));
     }
 }
