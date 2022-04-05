0530560d27f7aebbcfd146b4316150e7612ffa12
Liubov Chernohor <liubovchernohor@gmail.com>
2022-04-06 01:49:17 +0300

    The Hibernate framework was used in the cinema service

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+    private static final MovieService MOVIE_SERVICE = (MovieService) INJECTOR
+            .getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Taxi 25");
+        movie.setDescription("Spectacular movie");
+        System.out.println(MOVIE_SERVICE.add(movie));
+        System.out.println(MOVIE_SERVICE.get(movie.getId()));
     }
 }
