84f74d748535c1ba14b750fbbbb7bfdcccf0503d
Nazarii Yatsiuk <nazariiyatsiuk@gmail.com>
2022-04-06 02:40:18 +0300

    Hibernate was configured, classes were implemented

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,17 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
-
+        Injector injector = Injector.getInstance("mate");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("X-MEN");
+        movie.setDescription("Superhero movie");
+        movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
     }
 }
