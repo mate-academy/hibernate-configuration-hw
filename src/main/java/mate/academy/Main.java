052b4396986fddf9b30d0ab45cc9e45363a026b8
Nazarii Yatsiuk <nazariiyatsiuk@gmail.com>
2022-04-06 12:03:13 +0300

    Added empty line in the end of checkstyle.xml, replaced ккreference into аfile by url; replmoved ÐÐfields declaration out of PSVM method in Main class

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -5,8 +5,10 @@ import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
 
 public class Main {
+    private static Injector injector;
+    private static MovieService movieService;
     public static void main(String[] args) {
-        Injector injector = Injector.getInstance("mate");
+        Injector.getInstance("mate");
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         Movie movie = new Movie();
         movie.setTitle("X-MEN");
