ad305af08bcfa80ce0849e9063a4ebe2a6ecf971
AprioriKS <sounddummies@gmail.com>
2022-04-06 17:05:42 +0300

    hw solved

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
-    }
+public class Main {
+    public static Injector injector = Injector.getInstance("mate.academy");
+    public static void main(String[] args) {
+            MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+            Movie movie = new Movie("50 First Dates", "Henry Roth is a man afraid of"
+                + " commitment until he meets the beautiful Lucy. They hit it off and Henry think"
+                + " he's finally found the girl of his dreams until discovering she has "
+                + "short-term memory loss and forgets him the next day.");
+            movie = movieService.add(movie);
+            System.out.println(movieService.get(movie.getId()));
+        }
+
 }
