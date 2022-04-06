d6513e6ec70ec3615277538bc60d7a25c620b059
lugburz.hr <lugburz.hr@gmail.com>
2022-04-06 09:49:23 +0300

    Tasks done, works fine.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie1 = new Movie();
+        movie1.setTitle("Terminator 2");
+        movie1.setDescription("The only and the lonely credible!");
+        movieService.add(movie1);
+        Movie movie2 = new Movie();
+        movie2.setTitle("Insider");
+        movie2.setDescription("Quite good one!");
+        movieService.add(movie2);
+        Movie movieToGet = movieService.get(2L);
+        System.out.println(movieToGet);
     }
 }
