956aa8adc0c0ed7f31729c6bf436d7b4e3b88332
AprioriKS <sounddummies@gmail.com>
2022-04-07 10:10:30 +0300

    fixed

--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -53,10 +53,10 @@ public class Movie {
 
     @Override
     public String toString() {
-        return "Movie{" +
-            "id=" + id +
-            ", title='" + title + '\'' +
-            ", description='" + description + '\'' +
-            '}';
+        return "Movie{"
+            + "id=" + id
+            + ", title='" + title + '\''
+            + ", description='" + description + '\''
+            + '}';
     }
 }
