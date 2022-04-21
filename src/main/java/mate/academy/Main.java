4658190b2692e976dcca7e4af1e6027a4c619a41
Serhii Petrenko <Poshtasp76@gmail.com>
2022-04-21 10:45:06 +0300

    Hibernate configurations - first commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
     public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Terminator-2");
+        movie.setDescription("fantasy film");
+        movieService.add(movie);
+        Movie movie1 = movieService.get(1L);
+        System.out.println(movie1.toString());
 
     }
 }
---
+++ src/main/java/mate/academy/exception/DataProcessingException.java
@@ -0,0 +1,9 @@
+package mate.academy.exception;
+
+public class DataProcessingException extends RuntimeException {
+
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
