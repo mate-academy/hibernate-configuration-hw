47fc5fbf76026da8330705406e5c48e4d846aa16
JadeRowan <rougesoul.m@gmail.com>
2022-05-06 19:56:23 +0300

    Hyper comit for hyper mentor

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
-
+        MovieService movieService = (MovieService) Injector.getInstance("mate.academy")
+                .getInstance(MovieService.class);
+        Movie movie = new Movie("Drive", "Drive is a 2011 American action drama film " 
+                + "directed by Nicolas Winding Refn. The screenplay, written by Hossein Amini, " 
+                + "is based on James Sallis's 2005 novel of the same name.");
+        Movie added = movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
     }
+
 }
---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,44 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.exeption.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        Session session = null;
+        Transaction addTransaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            addTransaction = session.beginTransaction();
+            session.save(movie);
+            addTransaction.commit();
+        } catch (Exception e) {
+            if (addTransaction != null) {
+                addTransaction.rollback();
+                throw new DataProcessingException("Couldn't save movie " + movie, e);
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Movie movie = session.get(Movie.class, id);
+            return Optional.ofNullable(movie);
+        } catch (Exception e) {
+            throw new DataProcessingException("Couldn't save movie by id " + id, e);
+        }
+    }
+}
