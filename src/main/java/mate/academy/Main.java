07467b4bfc27d4a453c1a81a9d439f3e13e14ae6
Angela1608 <89303267+Angela1608@users.noreply.github.com>
2022-05-18 22:11:56 +0300

    solved hibernate conf task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,17 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    static Injector injector = Injector.getInstance("mate.academy");
+    public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Ocean");
+        movie.setDescription("Documentary");
+        movieService.add(movie);
     }
 }
