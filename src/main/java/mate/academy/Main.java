d74bab4717b2f09b586e2d71fc7d4e9e09a0638d
Andrii Fabersky <Andriifabersky@gmail.com>
2022-05-29 23:49:28 +0300

    i did homework

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static MovieService movieService
+            = (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Fast and Furious 9");
+        movie.setDescription("more Fast and Furious 9");
+        movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
     }
 }
