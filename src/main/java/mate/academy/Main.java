ed96f21cc07aaac3cc97e44ac7ff203ecfedaff2
Roman Kachmaryk <roman.kv98@gmail.com>
2022-05-30 17:32:08 +0300

    new commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,9 +6,9 @@ import mate.academy.service.MovieService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
     public static void main(String[] args) {
-        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         Movie movie = new Movie();
         movie.setTitle("The Godfather");
         movie.setDescription("Masterpiece");
