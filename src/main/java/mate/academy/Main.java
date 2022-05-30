1d532b353b8c5c573b29e952e9a327a6e5a25279
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-05-30 17:12:13 +0300

    main was has been changed

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        MovieService movieService = (MovieService) Injector.getInstance("mate.academy")
+                .getInstance(MovieService.class);
 
+        Movie movie = new Movie();
+        movie.setTitle("Lady hawk");
+        movie.setDescription("Lady hawke is a 1985 American medieval fantasy film directed and produced"
+                + " by Richard Donner and starring Matthew Broderick, Rutger Hauer and Michelle Pfeiffer");
+
+        Movie ladyHawk = movieService.add(movie);
+
+        System.out.println(movieService.get(ladyHawk.getId()));
     }
 }
