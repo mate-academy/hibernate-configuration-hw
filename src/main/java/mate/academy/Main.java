2b48a47e66e07eedabfe82fdb9a16e1ce7a1de5f
Maksym Karamavrov <makcumkaka@gmail.com>
2022-07-17 19:02:23 +0300

    First commit. Application runs clear

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
+import java.util.Optional;
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.model.Movie;
+
 public class Main {
     public static void main(String[] args) {
+        MovieDao movieDao = new MovieDaoImpl();
+        Movie movie = new Movie();
+        movie.setTitle("Fast and Furious 9");
+        movie.setDescription("Action 2020");
+        movieDao.add(movie);
 
+        Optional<Movie> movieFromDb = movieDao.get(1L);
+        System.out.println(movieFromDb);
     }
 }
