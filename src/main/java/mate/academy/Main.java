e531fa61978d10f101ce02682c6ad74f4c1505ca
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2022-07-21 20:40:32 +0300

    Implemented all configurations and classes according to README. Tested MovieServise class methods in main method

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
-
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movieTenet = new Movie();
+        movieTenet.setTitle("Tenet");
+        movieTenet.setDescription("Awesome mind blowing SCIFI movie!");
+        movieTenet = movieService.add(movieTenet);
+        Movie movieFromDb = movieService.get(1L);
+        System.out.println(movieFromDb);
+        System.out.println(movieService.get(2L));
     }
 }
