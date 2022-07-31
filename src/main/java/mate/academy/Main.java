d530eab62b5a65507907bdcb5ea672ea06fdeb57
drapail <mv.dibrov@gmail.com>
2022-07-31 19:46:41 +0300

    added service layer. Hibernate works now

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
+import mate.academy.lib.Inject;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+import mate.academy.service.MovieServiceImpl;
+
 public class Main {
+
+    private static final Injector injector = Injector.getInstance("mate");
+    private static MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
     public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setDescription("Cool movie description #1");
+        movie.setTitle("Cool movie #1");
+        movieService.add(movie);
 
     }
 }
--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,12 +1,24 @@
 package mate.academy.dao;
 
+import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.Optional;
 
+@Dao
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(movie);
+        transaction.commit();
+        session.close();
         return null;
     }
 
