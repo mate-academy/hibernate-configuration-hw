15ed472d79c9bad594b41f95d9fd8f53a5a7cb07
Vitaliy Zahrenko <vzagorenko@gmail.com>
2022-08-15 22:39:10 +0300

    Main method has been fixed

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,17 +6,13 @@ import mate.academy.service.MovieService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
-
     public static void main(String[] args) {
         MovieService movieService =
                 (MovieService) injector.getInstance(MovieService.class);
         Movie movie = new Movie();
-
         movie.setTitle("pulp fiction");
         movie.setDescription("The best Tarrantino film");
         movie = movieService.add(movie);
         Movie outMovie = movieService.get(movie.getId());
     }
-
 }
-
