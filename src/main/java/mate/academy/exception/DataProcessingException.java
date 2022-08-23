8ecbf94fb64c6678a19321dee42120379befd949
Ilya <zimmboco@gmail.com>
2022-08-23 11:14:41 +0300

    add DAO, service, exception

--- src/main/java/mate/academy/exception/DataProcessingException.java
+++ src/main/java/mate/academy/exception/DataProcessingException.java
@@ -1,7 +1,7 @@
 package mate.academy.exception;
 
 public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String  messages, Throwable cause) {
+    public DataProcessingException(String messages, Throwable cause) {
         super(messages, cause);
     }
 }
