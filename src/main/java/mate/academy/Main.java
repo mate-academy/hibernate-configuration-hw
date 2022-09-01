9e2ebf4e1549564b9bdfeda1018227fae87c2c51
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-09-01 16:28:07 +0200

    Hw-is-done

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Titanic");
+        movie.setDescription("Titanic is a 1997 American epic romantic disaster movie.");
+        movieService.add(movie);
+        System.out.println(movieService.get(1L));
     }
 }
