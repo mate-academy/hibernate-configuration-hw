431e5a281009e9f3fb6fee393654b29951cd2b8b
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-09-14 18:15:14 +0300

    Homework is done

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
-
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie terminator = new Movie();
+        terminator.setTitle("Terminator");
+        terminator.setDescription("Classic movie from 1984.");
+        terminator = movieService.add(terminator);
+        System.out.println(terminator.getId());
+        System.out.println(movieService.get(1L));
     }
 }
