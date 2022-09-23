7d2b3472c4062a96de4447369e02aef48e37ad2d
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-09-23 15:22:47 +0300

    add dependencies to pom.xml, create hibernate.cfg.xml, create HibernateUtil class and model Movie. create intrefaces MovieDao, MovieService and implementions MovieDaoImpl and MovieServiceImpl. create exception DataProcessingException.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = (Injector) Injector.getInstance("mate");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie taxi = new Movie();
+        taxi.setTitle("Taxi");
+        taxi.setDescription("Very speed movie");
+
+        movieService.add(taxi);
+
+        Movie movie = movieService.get(1L);
+        System.out.println(movie);
     }
 }
