2c4a2310e17982b3dbb3f19e057274bdbf1e587f
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-09-27 00:06:36 +0300

    MovieDao and MovieDaoImpl were added

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,25 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(movie);
+        transaction.commit();
+        session.close();
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        return Optional.ofNullable(session.get(Movie.class, id));
+    }
+}
