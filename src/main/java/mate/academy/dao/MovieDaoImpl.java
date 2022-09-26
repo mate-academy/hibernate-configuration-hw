09522732b86cb5e3f1e27e5dd80ae38cc8213757
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-09-27 00:30:57 +0300

    MovieService, MovieServiceImpl, DataProcessingException were added

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,11 +1,14 @@
 package mate.academy.dao;
 
 import java.util.Optional;
+
+import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+@Dao
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
