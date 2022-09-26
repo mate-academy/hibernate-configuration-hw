b2bf5365b3384c84e9041a783d93b4cd883da06a
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-26 22:06:53 +0300

    impl. method add in MovieDaoImpl

--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -4,12 +4,22 @@ import java.util.Optional;
 import mate.academy.dao.MovieDao;
 import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
-        return null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(movie);
+        transaction.commit();
+        session.close();
+        return movie;
     }
 
     @Override
