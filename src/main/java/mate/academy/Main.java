da51f4a485b3c3aa0dc8695d0e78eb6023dfc620
Valentyn Chepiha <valentyn.chepiha@gmail.com>
2022-09-27 01:56:16 +0300

    Added solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,25 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
+
+        Movie movie = new Movie();
+        movie.setTitle("GoT");
+        movie.setDescription("Story about love, politic and war");
+
+        System.out.println("Step 1. Save data");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(movie);
+        System.out.println(movie);
+
+        System.out.println("Step 2. Read");
+        System.out.println(movieService.get(movie.getId()));
 
     }
 }
