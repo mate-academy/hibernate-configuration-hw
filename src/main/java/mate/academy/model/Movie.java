710a1fbf5442e8ea58a98bf095d3debdf1deb41c
fliarkovskyi <fliarkovskyi@gmail.com>
2022-09-27 15:08:37 +0300

    change Movie

--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "movies")
@@ -40,9 +41,28 @@ public class Movie {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Movie movie = (Movie) o;
+        return Objects.equals(id, movie.id)
+                && Objects.equals(title, movie.title)
+                && Objects.equals(description, movie.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description);
+    }
+
+    @Override
     public String toString() {
         return "Movie{"
-                + "id=" + id + '\''
+                + "id=" + id
                 + ", title='" + title + '\''
                 + ", description='" + description + '\''
                 + '}';
