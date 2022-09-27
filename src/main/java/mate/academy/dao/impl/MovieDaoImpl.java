05ca91dbdc1efa7dbcd88d2da4e2d7dbd598ddbb
Alexhqolma <66127994+Alexhqolma@users.noreply.github.com>
2022-09-27 14:20:31 +0300

    Corrected session close

--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -37,8 +37,12 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public Optional<Movie> get(Long id) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        return Optional.ofNullable(session.get(Movie.class, id));
+        try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            session.close();
+            return Optional.ofNullable(session.get(Movie.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't save movie " + " to DB", e);
+        }
     }
 }
