c552ac9231a287c01c5f5fbb49161a562453d8a5
fliarkovskyi <fliarkovskyi@gmail.com>
2022-09-27 14:44:51 +0300

    change hibernate.cfg

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -5,15 +5,13 @@ import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
 
 public class Main {
-    private static final Injector injector = Injector.getInstance("mate.academy");
-
     public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         Movie movie = new Movie();
-        movie.setTitle("avanger");
+        movie.setTitle("avengers");
         movie.setDescription("type 2");
         movieService.add(movie);
-
         System.out.println(movieService.get(1L));
     }
 }
