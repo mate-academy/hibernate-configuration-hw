5f310180918987b8b1189d4c48688db517ae8c78
LubusUzh <lubus.uzh@gmail.com>
2022-09-27 15:07:05 +0300

    Add: create Hibernate configuration, add classes, interfacea for dao and service levels, custom exception

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        final MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("LOTR: The Return of the King");
+        movie.setDescription("One of the best fantasy movies of all times");
+        movieService.add(movie);
+        Movie getMovie = movieService.get(1L);
+        System.out.println(getMovie);
     }
 }
