f85c6b69c403999d559cc58bf8d8e205fbf2761a
Aptypio13 <aptypio668@gmail.com>
2022-09-27 14:54:30 +0300

    Create DAO and Service for ticket_app

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector =
+            Injector.getInstance("mate.academy");
+    private static MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Pockemons");
+        movie.setDescription("Afoul");
+        System.out.println(movieService.add(movie));
+        System.out.println(movieService.get(movie.getId()).getTitle());
+        System.out.println(movieService.get(movie.getId()).getId());
+        System.out.println(movieService.get(movie.getId()).getDescription());
     }
 }
