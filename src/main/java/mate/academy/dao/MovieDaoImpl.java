ba7bc7ca820e85f7bcc0310f43e47bf2e331cd93
Aleksandr Savchenko <savchenko.arts@gmail.com>
2022-10-11 16:20:16 +0300

    hibernate-configuration-hw

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,16 @@
+package mate.academy.dao;
+
+import mate.academy.model.Movie;
+import java.util.Optional;
+
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        return null;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,16 @@
+package mate.academy.service;
+
+import mate.academy.model.Movie;
+
+public class MovieServiceImpl implements MovieService {
+
+    @Override
+    public Movie add(Movie movie) {
+        return null;
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return null;
+    }
+}
