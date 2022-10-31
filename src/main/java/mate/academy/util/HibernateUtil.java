60f1b27ce3940c88a19086ca61ac8b2e46bbf943
Oleksandr Kravchenko <Pokemonishche@me.com>
2022-10-31 23:48:59 +0200

    save temp

---
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -0,0 +1,19 @@
+package mate.academy.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private HibernateUtil() {
+    }
+
+    private static final SessionFactory instance = initSessionFactory();
+
+    private static SessionFactory initSessionFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return instance;
+    }
+}
