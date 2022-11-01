31cdbecf16d20cb49eb97fceab1c80bdfd8c4cf2
Oleksandr Kravchenko <Pokemonishche@me.com>
2022-11-01 14:17:06 +0200

    hw done

--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -39,10 +39,9 @@ public class Movie {
 
     @Override
     public String toString() {
-        return "Movie{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                '}';
+        return "Movie{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", description='" + description + '\'' + '}';
     }
 }
