74aa3bb7c2883251e21ceb953766764941b8d03e
shmax <mshveda28@gmail.com>
2022-11-14 22:18:12 +0200

    Add private default constructor to HibernateUtil to prevent creating object of this class

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -6,6 +6,8 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
 
+    private HibernateUtil() {}
+
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             sessionFactory = new Configuration().configure().buildSessionFactory();
