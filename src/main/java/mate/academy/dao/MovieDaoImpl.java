8e92a0b1bb0d0b15296df95bdca12b4795b0f6e6
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-11-14 20:35:38 +0200

    Add correct return

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.dao;
 
 import java.util.Optional;
+import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
@@ -13,22 +14,24 @@ public class MovieDaoImpl implements MovieDao {
     public Movie add(Movie movie) {
         Session session = null;
         Transaction transaction = null;
-        Movie savedMovie = null;
+        Long id = null;
         try {
             session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
-            savedMovie = (Movie) session.save(movie);
+            id = (Long) session.save(movie);
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
+            throw new DataProcessingException("Error occurs while adding the movie!", e);
         } finally {
             if (session != null) {
                 session.close();
             }
         }
-        return savedMovie;
+        movie.setId(id);
+        return movie;
     }
 
     @Override
