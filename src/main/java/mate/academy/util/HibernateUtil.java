4e14bfe88ae6c5d65ae3b086c4902645c07a9b7c
Fokin <Fokin1@ukr.net>
2022-11-14 05:28:56 +0100

    fix mistake

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -1,16 +1,19 @@
-package mate.academy.utill;
+package mate.academy.util;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-     private static final SessionFactory instance = initSessionFactory();
+    private static final SessionFactory instance = initSessionFactory();
 
-     private static SessionFactory initSessionFactory() {
-         return new Configuration().configure().buildSessionFactory();
-     }
+    private HibernateUtil() {
+    }
 
-     public static SessionFactory getSessionFactory() {
-         return instance;
-     }
+    private static SessionFactory initSessionFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return instance;
+    }
 }
