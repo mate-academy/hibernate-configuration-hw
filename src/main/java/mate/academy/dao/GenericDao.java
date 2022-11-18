d29d9b2684ccc5c11bf3c2e0ebc9afa32c0828cd
Artur Markoboh <arthur4655@gmail.com>
2022-11-18 20:30:16 +0200

    MovieDao interface was implemented

--- src/main/java/mate/academy/dao/GenericDao.java
+++ src/main/java/mate/academy/dao/GenericDao.java
@@ -4,5 +4,6 @@ import java.util.Optional;
 
 public interface GenericDao<T, P> {
     T add(T o);
+
     Optional<T> get(P id);
 }
--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -2,18 +2,41 @@ package mate.academy.dao.impl;
 
 import java.util.Optional;
 import mate.academy.dao.MovieDao;
+import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
     @Override
-    public Movie add(Movie o) {
-        return null;
+    public Movie add(Movie movie) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(movie);
+            transaction.commit();
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add movie " + movie + " to DB.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public Optional<Movie> get(Long id) {
-        return Optional.empty();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Movie movie = session.get(Movie.class, id);
+        return Optional.ofNullable(movie);
     }
 }
