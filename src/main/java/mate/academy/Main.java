954973ca6ae27a335e409102f78798d7f4d808db
Pavlo.Polovyi <pasha32662@gmail.com>
2022-11-27 13:43:04 +0200

    Implemented all tasks

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Lord of the rings");
+        movie.setDescription("Fantasy film about ...");
+        Movie lordOfTheRings = movieService.add(movie);
+        Long id = lordOfTheRings.getId();
+        System.out.println(id);
+        Movie lotrFromDb = movieService.get(id);
+        System.out.println(lotrFromDb);
     }
 }
