fb1bbe04ffe9692d2917b27f6f08c73558f18e10
MuzykaVP <muzykavp.work@gmail.com>
2022-11-29 23:51:06 +0200

    Hibernate configuration setup

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        movieService.add(new Movie("Shrek","Very funny movie about ogre"));
+        movieService.add(new Movie("Mad Max","He's Max and he's MAD"));
+        movieService.add(new Movie("Scary Movie 2","Unbelievably scary movie"));
+        Movie movie = movieService.get(1L);
+        Movie movie1 = movieService.get(3L);
     }
 }
