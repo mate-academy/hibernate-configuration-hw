72e5c56d5d3b745a489d66f0c4458179e15a3cee
Bodddd <skrypnikbod@gmail.com>
2022-11-30 19:24:43 +0200

    starting working with Hibernate

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Lucky man");
+        movie.setDescription("Good film");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
     }
 }
