69d53ed2f01c63deba74a1e365ffbfaec6715ad4
Veronika Tretiak <nikatretiak9@gmail.com>
2022-11-30 22:55:26 +0100

    Implemented task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate");
+        MovieService movieService
+                = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Matrix");
+        movie.setDescription("The world of the Matrix is an illusion.");
+        movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
 
     }
 }
