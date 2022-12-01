a8b42a779b4304400764c618420c55b0a3929ae3
Oleksiy <a.o.masaltsev@gmail.com>
2022-12-01 12:12:01 +0200

    HibernateUtil & Movie class are created

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,31 @@
+package mate.academy.dao;
+
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.Optional;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(movie);
+        transaction.commit();
+        session.close();
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        return Optional.ofNullable(session.get(Movie.class, id));
+    }
+}
