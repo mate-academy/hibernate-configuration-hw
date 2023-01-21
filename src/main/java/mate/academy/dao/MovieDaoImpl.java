1ab2179ba4ff3a9dbb8394246c151ce42a224fff
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-01-21 17:04:16 +0100

    redone daoImpl

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,31 +1,32 @@
 package mate.academy.dao;
 
-import java.util.Objects;
 import java.util.Optional;
 import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+@Dao
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
         Session session = null;
         Transaction transaction = null;
         try {
-            session = getSession();
+            session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
             session.save(movie);
             transaction.commit();
         } catch (Exception e) {
-            if (Objects.nonNull(transaction)) {
+            if (transaction != null) {
                 transaction.rollback();
+                throw new DataProcessingException(String.format("Can't save %s to DB", movie), e);
             }
-            throw new DataProcessingException(String.format("Can't save %s to DB", movie), e);
         } finally {
-            if (Objects.nonNull(session)) {
+            if (session != null) {
                 session.close();
             }
         }
@@ -34,15 +35,10 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public Optional<Movie> get(Long id) {
-        try (Session session = getSession()) {
-            return Optional.ofNullable(session.get(Movie.class, id));
-        } catch (Exception e) {
-            throw new DataProcessingException(String.format("Can't get movie by %d", id), e);
-        }
-    }
-
-    private Session getSession() {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        return sessionFactory.openSession();
+        Session session = sessionFactory.openSession();
+        Optional<Movie> movie = Optional.ofNullable(session.get(Movie.class, id));
+        session.close();
+        return movie;
     }
 }
