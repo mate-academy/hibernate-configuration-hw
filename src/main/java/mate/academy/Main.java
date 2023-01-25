7b2ce81633a163ebb88381638dfbbaaa561dd156
Maksym Tymoshchenko <timoschenkomaks123@gmail.com>
2023-01-25 22:57:00 +0200

    first commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
-
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie firstMovie = movieService
+                .add(new Movie("The Green Mile", "He was a good guy"));
+        Movie secondMovie = movieService
+                .add(new Movie("Star wars", "Just a good one"));
+        System.out.println(movieService.get(firstMovie.getId()).toString()
+                + movieService.get(secondMovie.getId()).toString());
     }
 }
