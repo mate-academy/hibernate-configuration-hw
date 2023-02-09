573674c242a59ad530fafcba6646e97e792c7243
Inna Diachkina <innadyachkina@gmail.com>
2023-02-09 20:06:03 +0200

    my solved

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -36,7 +36,6 @@ public class MovieDaoImpl implements MovieDao {
     public Optional<Movie> get(Long id) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Movie movie = session.get(Movie.class, id);
-            session.close();
             return Optional.ofNullable(movie);
         }
     }
