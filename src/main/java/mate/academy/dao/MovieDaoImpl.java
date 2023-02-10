105abd6e0890557d0bc915344245025b5e110411
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2023-02-10 15:47:30 +0200

    second

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,23 +1,37 @@
-package mate.academy.dao.impl;
+package mate.academy.dao;
 
 import java.util.Optional;
-import mate.academy.dao.MovieDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+@Dao
 public class MovieDaoImpl implements MovieDao {
 
     @Override
     public Movie add(Movie movie) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(movie);
-        transaction.commit();
-        session.close();
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+        } catch (DataProcessingException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add movie to DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
         return movie;
     }
 
