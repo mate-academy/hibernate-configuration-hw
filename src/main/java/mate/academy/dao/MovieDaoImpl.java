8bebe2b37bbac02cc14e49a5e2a9dc052bced380
Serhii Berdychevskyi <sirko120690@mail.com>
2023-02-10 13:06:01 +0200

    Added 1.0 - version hibernate-config

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.dao;
 
+import java.util.Optional;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
@@ -8,10 +9,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-import java.util.Optional;
-
 @Dao
-public class MovieDaoImpl implements MovieDao{
+public class MovieDaoImpl implements MovieDao {
     private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
     @Override
