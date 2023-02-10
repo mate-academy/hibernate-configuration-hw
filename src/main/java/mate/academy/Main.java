c197d28f4f26dfabe1a39b14ce19a29606ec5761
ydikalo <dikalotel@gmail.com>
2023-02-10 21:08:36 +0200

    fixed main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -5,13 +5,11 @@ import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
 
 public class Main {
-    private static final Injector injector = Injector.getInstance("mate.academy");
-    private static final MovieService movieService =
-            (MovieService) injector.getInstance(MovieService.class);
-
     public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
         Movie movie = new Movie();
-        movie.setId(1L);
         movie.setTitle("Shrek Forever After");
         movie.setDescription("movie i'm about to rewatch after i'm done with this task");
         System.out.println(movieService.add(movie));
