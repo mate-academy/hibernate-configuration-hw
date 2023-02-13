1da05db89cb16f9471fa002c989384d24bb7b385
Pandoryms <andrei.hlebov21@gmail.com>
2023-02-13 12:09:51 +0200

    update

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,14 +6,15 @@ import mate.academy.service.MovieService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
 
-    public static void main(String[] args) {
-        MovieService movieService = (MovieService) injector
-                .getInstance(MovieService.class);
+    public static void main(String[]args) {
         Movie movie = new Movie();
         movie.setTitle("Moana");
         movie.setDescription("Cartoon!");
         movieService.add(movie);
-        movieService.get(1L);
+        Long id = movie.getId();
+        System.out.println(movieService.get(id));
     }
 }
