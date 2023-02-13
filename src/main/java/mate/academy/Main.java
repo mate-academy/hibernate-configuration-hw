c39c4d7d1bf6c21b792485e3a726021d8acc0c99
Pandoryms <andrei.hlebov21@gmail.com>
2023-02-13 10:05:16 +0200

    update

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -3,13 +3,10 @@ package mate.academy;
 import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
-import mate.academy.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
         MovieService movieService = (MovieService) injector
                 .getInstance(MovieService.class);
