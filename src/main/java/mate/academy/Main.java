6dcb37610732b67c7313ee082a35e13a7ea45f83
Your NameMykola <mykola.osolinskyi@gmail.com>
2023-02-19 13:42:47 +0100

    build success

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,15 +1,21 @@
 package mate.academy;
 
 import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
 
 public class Main {
-    private static final Injector injector =
-            Injector.getInstance("mate.academy");
-    private final MovieService movieService =
-            (MovieService) injector.getInstance(MovieService.class);
-
     public static void main(String[] args) {
+        Injector injector =
+                Injector.getInstance("mate.academy");
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("The Gentlemen");
+        movie.setDescription("One of the best movies all time");
+
+        System.out.println(movieService.add(movie));
+        System.out.println(movieService.get(movie.getId()));
 
     }
 }
--- src/main/java/mate/academy/service/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -1,17 +1,22 @@
 package mate.academy.service;
 
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.Movie;
 
 @Service
 public class MovieServiceImpl implements MovieService {
+    @Inject
+    private MovieDao movieDao;
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        return movieDao.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        return movieDao.get(id).orElseThrow();
     }
 }
