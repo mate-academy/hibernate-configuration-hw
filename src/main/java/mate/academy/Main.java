16b67a3379b834b1bb90524b384cfddf7c4e1552
Sander <alex.kapusniak@gmail.com>
2023-02-21 19:36:52 +0200

    work version

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        Injector injector =
+                Injector.getInstance("mate.academy");
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("The Gentlemen");
+        movie.setDescription("One of the best movies all time");
+
+        System.out.println(movieService.add(movie));
+        System.out.println(movieService.get(movie.getId()));
 
     }
 }
