77005a9ae7007f8fc0cde3445775413daa6a79ac
Denis Pugovkin <den.pugovkin@gmail.com>
2023-02-24 12:21:27 +0200

    1. Added all necessary dependencies. 2. Created and configured Hibernate. 3. Created class Movie and Dao and Service methods for it. 4. Created custom exception for Dao-layer. 5. Created test launch in main method.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("One Flew Over the Cuckoo's Nest");
+        movie.setDescription("It's a 1975 American psychological comedy drama film");
+        movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
     }
 }
