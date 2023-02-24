f3f282c86e3c23b0e7fdcddabef5143c4612fda3
carbo <carbon7557@gmail.com>
2023-02-24 19:22:06 +0000

    /

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+        private static final Injector injector = Injector.getInstance("mate.academy");
+
+        public static void main(String[] args) {
+            MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+            Movie movie = new Movie();
+            movie.setTitle("Pirates of the Caribbean");
+            movie.setDescription("American fantasy supernatural swashbuckler film");
+            movieService.add(movie);
+            Movie movieFromDB = movieService.get(1L);
+            System.out.println(movieFromDB);
+        }
     }
-}
