74d429fd06c7d2fc18d7116474bb64fe364cfbd3
Alona <alenavyrodova@gmail.com>
2023-03-07 12:16:21 +0200

    add annotation

--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "movies")
+@Table(name = "ticket_app")
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
