5bc1036196276141449356cc1a22799aaca5a437
Vasyl Piznak <piznak3145@gmail.com>
2023-03-21 21:15:18 +0200

    BUILD SUCCESS

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -10,7 +10,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 @Dao
-public class MovieDaoIml implements MovieDao {
+public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
@@ -21,7 +21,7 @@ public class MovieDaoIml implements MovieDao {
             transaction = session.beginTransaction();
             session.save(movie);
             transaction.commit();
-        } catch (DataProcessingException e) {
+        } catch (RuntimeException e) {
             if (transaction != null) {
                 transaction.rollback();
             }
