df044de2442c22609a6a4b746e7893e4c675a9ee
Sergii Uglovyi <s.uglovyi@gmail.com>
2023-03-22 15:29:41 +0200

    add solution

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,48 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(movie);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("DAO error when saving a movie " + movie, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Movie movie = null;
+        try (Session session = sessionFactory.openSession()) {
+            movie = session.get(Movie.class, id);
+        } catch (RuntimeException e) {
+            throw new DataProcessingException("DAO error when receiving a movie by id " + id, e);
+        }
+        return movie == null ? Optional.empty() : Optional.of(movie);
+    }
+}
