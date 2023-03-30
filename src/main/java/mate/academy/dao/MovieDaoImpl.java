5a6cc8d83b4fd5f5808c76c769211c851710295e
sasha <radovietsoleksandr@gmail.com>
2023-03-30 03:26:46 +0300

    ww5

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.dao;
 
 import java.util.Optional;
+import mate.academy.exeption.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
@@ -25,7 +26,7 @@ public class MovieDaoImpl implements MovieDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("it is method add not correct ", e);
+            throw new DataProcessingException("it is method add not correct ", e);
         } finally {
             if (session != null) {
                 session.close();
@@ -38,6 +39,7 @@ public class MovieDaoImpl implements MovieDao {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         Session session = sessionFactory.openSession();
         Movie movie = session.get(Movie.class, id);
+        session.close();
         return Optional.ofNullable(movie);
     }
 }
