1f2c4642d2b4e86990b221bc91b89927df88c826
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-03-31 15:38:22 +0300

    create MovieDao and fix some configuration

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
 
+public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieDao movieDao = (MovieDao) INJECTOR.getInstance(MovieDao.class);
+        Movie movie = new Movie();
+        movie.setTitle("Jack");
+        movie.setDescription("good");
+        movieDao.add(movie);
+
+        System.out.println(movieDao.get(1L));
     }
 }
