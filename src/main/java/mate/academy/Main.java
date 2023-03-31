4a354c76d50a32ae335441a2cc57a0e3c922cd54
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-03-31 16:06:46 +0300

    created exception and tested in main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,19 +1,20 @@
 package mate.academy;
 
-import mate.academy.dao.MovieDao;
 import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
 public class Main {
     private static final Injector INJECTOR = Injector.getInstance("mate.academy");
 
     public static void main(String[] args) {
-        MovieDao movieDao = (MovieDao) INJECTOR.getInstance(MovieDao.class);
+        MovieService movieService = (MovieService) INJECTOR.getInstance(MovieService.class);
+
         Movie movie = new Movie();
         movie.setTitle("Jack");
         movie.setDescription("good");
-        movieDao.add(movie);
 
-        System.out.println(movieDao.get(1L));
+        movieService.add(movie);
+        System.out.println(movieService.get(1L));
     }
 }
