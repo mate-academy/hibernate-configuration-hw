ca5803abe7af201a08172ad2753ba6b43f6dacc7
Ilia Budenkov <i.budenkov@gmail.com>
2023-04-03 21:57:22 +0300

    added solution to the task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
-
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Dungeons & Dragons: Honor Among Thieves ");
+        movie.setDescription("A charming thief and a band of unlikely adventurers"
+                + " undertake an epic heist to retrieve a lost relic");
+        movieService.add(movie);
+        movie = new Movie();
+        movie.setTitle("John Wick: Chapter 4");
+        movie.setDescription("John Wick (Keanu Reeves) uncovers "
+                + "a path to defeating The High Table");
+        movieService.add(movie);
+        movie = movieService.get(1L);
+        System.out.println(movie);
+        movie = movieService.get(2L);
+        System.out.println(movie);
     }
 }
