77445d2fce5a9010bbf4a6e856953fe316898aa7
Denys Dominskyi <dominskyj@gmail.com>
2023-04-06 16:31:45 +0300

    Initial commit

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,39 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.save(movie);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            throw new DataProcessingException("The transaction was not completed!");
+        } finally {
+            transaction.rollback();
+            session.close();
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Optional<Movie> movie = Optional.ofNullable(session.get(Movie.class, id));
+        session.close();
+        return movie;
+    }
+}
