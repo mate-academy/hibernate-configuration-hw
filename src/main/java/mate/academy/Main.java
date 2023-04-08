a43bc23bc72f27f360056cb4d64eea8d7149878c
o-shyshkan <o.shyshkan@gmail.com>
2023-04-08 09:26:15 +0300

    Hibernate configuration

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector
+                .getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Terminator 1");
+        movie.setDescription("Action movie.");
+        movieService.add(movie);
+        Movie movieTest = new Movie();
+        movieTest = movieService.get(1L);
+        System.out.printf("Title:", movieTest.getTitle());
+        System.out.printf("Description:", movieTest.getDescription());
 
     }
 }
