4b9f47643c03065d13e34979879119353f168ba9
KosMarch <kosmarchenko98@gmail.com>
2023-04-11 21:04:49 +0300

    added Movie, HibernateUtil, classes, DAO, cfg

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
+        MovieDao movieDao = new MovieDaoImpl();
 
+        Movie movie = new Movie();
+        movie.setId(10L);
+        movie.setTitle("Ninja turtles 2");
+        movie.setDescription("cartoon");
+
+        movieDao.save(movie);
+
+        Optional<Movie> movieFromDB = movieDao.get(1L);
+        System.out.println(movieFromDB);
     }
 }
---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,33 @@
+package mate.academy.dao;
+
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import java.util.Optional;
+
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie save(Movie movie) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(movie);
+        transaction.commit();
+        session.close();
+        return movie;
+    }
+
+    @Override
+    public Movie add(Movie movie) {
+        return null;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        return Optional.ofNullable(session.get(Movie.class, id));
+    }
+}
