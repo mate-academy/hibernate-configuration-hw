ba65f4f72308972d62ff9e837bf3bbb7d69206d4
Oleh Sarapuk <olehsarapuk@gmail.com>
2023-04-12 18:07:16 +0300

    implemented higernate configuration

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,17 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.MovieService;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
 
+public class Main {
+    private static Injector injector = Injector.getInstance("mate");
+    private static MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie("Spotlight", "Incredible");
+        System.out.println("add" + movieService.add(movie));
+        System.out.println("get" + movieService.get(movie.getId()));
     }
 }
---
+++ src/main/java/mate/academy/dao/impl/MovieServiceImpl.java
@@ -0,0 +1,24 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+
+@Service
+public class MovieServiceImpl implements mate.academy.dao.MovieService {
+    @Inject
+    private MovieDao movieDao;
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id).orElseThrow(() ->
+            new DataProcessingException("there is no movie with such id " + id));
+    }
+}
