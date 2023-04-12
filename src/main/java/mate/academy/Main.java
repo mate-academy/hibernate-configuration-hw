456938fb91ede715fe16d68a7cd5823658ba002f
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-04-12 12:40:14 +0300

    Implemented solution for HW.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("John Wick");
+        movie.setDescription("An ex-hit-man comes out of retirement to track down the gangsters"
+                + " that killed his dog and took his car.");
+        movie = movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
     }
 }
