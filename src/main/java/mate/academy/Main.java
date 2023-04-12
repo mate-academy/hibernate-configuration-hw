57edac0725122acd9bf672a207d39782bf4abf40
Nickolay Voloshchuk <124300295+NickolayVoloshchuk@users.noreply.github.com>
2023-04-12 14:35:07 +0300

    HomeWork Java/Hibernate Configuration

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie("Terminator 10", "new history of GPT uprising!");
+        Movie expected = movieService.add(movie);
+        Movie actual = movieService.get(expected.getId());
+        System.out.println("expected: " + expected);
+        System.out.println("actual  : " + actual);
+        System.out.println(expected.equals(actual) ? "Test successful!" : "Test fall!");
     }
 }
