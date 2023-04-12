4d245be8312cc51cf973224a2bf32c57a9d18bf1
a.ponomarenko-at-201540164075 <NF1Y9pEm016SCxSUCNnwBwHKLQD+CU8N9IuhiFm/FsA=>
2023-04-12 21:28:26 +0300

    initial commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,16 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.movie.MovieService;
+
 public class Main {
     public static void main(String[] args) {
-
+        final MovieService movieService
+                = (MovieService) Injector.getInstance("mate.academy")
+                .getInstance(MovieService.class);
+        Movie movie = new Movie("Titanic", "romance, drama");
+        movieService.add(movie);
+        movieService.get(movie.getId());
     }
 }
