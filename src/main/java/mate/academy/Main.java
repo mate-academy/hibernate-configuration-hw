caa030f10df75f7f512b33da3c92c38230036514
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-04-12 18:58:24 +0300

    Implemented hibernate configuration

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Avatar: the way of water");
+        movie.setDescription("Fantastic movie");
+        System.out.println(movieService.add(movie));
+        System.out.println(movieService.get(1L));
     }
 }
