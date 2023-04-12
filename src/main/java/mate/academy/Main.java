bc33c032d516b8535961965ec02d229cb3e19fa3
Your Name <you@example.com>
2023-04-12 14:53:27 +0300

    HibernateUtil added, Movie class added, Dao, Service layer added

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieDao movieDao =
+            (MovieDao) injector.getInstance(MovieDao.class);
+
+    public static void main(String[] args) {
+        Movie movie1 = new Movie();
+        movie1.setTitle("Fast and Furious 9");
+        movie1.setDescription("Awesome movie");
+        System.out.println(movieDao.add(movie1));
+        System.out.println(movieDao.get(movie1.getId()));
     }
 }
