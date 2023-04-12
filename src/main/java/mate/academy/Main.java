b557fce7c1306408724716ab009ebaef85845a6d
Mariana Polishchuk <tonksmaryan@gmail.com>
2023-04-12 17:11:35 +0300

    add Hibernate configuration

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector
+            = Injector.getInstance("mate.academy");
+    private static MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("The Shawshank Redemption");
+        movie.setDescription("Over the course of several years, two convicts form a friendship,"
+                + " seeking consolation and, eventually, redemption through basic compassion.");
+        movieService.add(movie);
+        System.out.println("Movie number one:" + movieService.get(1L));
     }
 }
