ef35b667a1a8a8704550c6c9725f88f8b4fa99fc
Evhenii Vaskiv <evgenijvaskiv@gmail.com>
2023-04-27 14:59:06 +0300

    error occurred

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate");
+    private static MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Inventing Anna");
+        movie.setDescription("Nice series");
+        movieService.add(movie);
+        Movie movie1 = movieService.get(1L);
+        System.out.println(movie1.getDescription());
     }
 }
