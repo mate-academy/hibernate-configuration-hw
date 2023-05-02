01bd8b49034e32920100de72e2e9162367a041a7
Artur Muliukov <arthur.muliukov@gmail.com>
2023-05-02 19:50:12 +0200

    Solved 1.0

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 public class Main {
     public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Batmen");
+        movie.setDescription("about batman");
 
+        MovieDao movieDao = new MovieDaoImpl();
+        movieDao.add(movie);
+        System.out.println(movieDao.get(1L));
     }
 }
