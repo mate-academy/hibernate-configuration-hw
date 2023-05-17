dbfc0d78abf1ae3e3b98f8950e5dee4297f281a6
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-05-17 18:41:35 +0300

    all requirements from readme are met

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,22 +1,24 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
-import mate.academy.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import mate.academy.service.MovieService;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
 
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
         Movie movie = new Movie();
-        movie.setTitle("Fast and Furious 9");
-        session.save(movie);
-        transaction.commit();
-        session.close();
+        movie.setTitle("terminator");
+        movie.setDescription("fantastic");
+
+        movieService.save(movie);
+
+        Movie movieFromDB = movieService.get(1L);
+        System.out.println(movieFromDB);
+
     }
 }
