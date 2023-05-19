6604cc0e2c70c054ba388710355ce5e2aa083817
Oles <a.vykhovanok@gmail.com>
2023-05-19 17:24:29 +0300

    solved task

---
+++ src/main/java/mate/academy/model/Movie.java
@@ -0,0 +1,21 @@
+package mate.academy.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String title;
+    private String description;
+}
