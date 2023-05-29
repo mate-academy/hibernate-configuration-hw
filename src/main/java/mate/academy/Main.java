af37b3970e68cadeef2d4823a2ea10e01b6199b1
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2023-05-29 23:41:11 +0300

    valid solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Flash");
+        movie.setDescription("very interesting movie");
+        movieService.add(movie);
+
+        Movie movieGet = movieService.get(1L);
+        System.out.println(movieGet);
+
+        Movie movieAdd = movieService.get(1L);
+        System.out.println(movieAdd);
     }
 }
