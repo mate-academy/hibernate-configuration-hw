cfc89bd4776e8ddac9397346dd30d2870fb01b26
Dmytro Katrevych <katrevichdv@gmail.com>
2023-06-12 17:44:21 +0300

    Dependencies were added. hibernate.cfg.xml, HibernateUtil, Movie model class, MovieDao, MovieDaoImpl, MovieService, MovieServiceImpl, DataProcessingException were created. main() method was implemented.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
-
+        Injector injector =
+                Injector.getInstance("mate.academy");
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+        Movie movie1 = new Movie();
+        movie1.setTitle("Dune");
+        movie1.setDescription("Dune: Part One");
+        movieService.add(movie1);
+        Movie movie2 = movieService.get(1L);
+        System.out.println(movie2);
     }
 }
