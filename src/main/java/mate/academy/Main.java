fec3086d65224e4d2928efa75d3aa5b53669b482
Ilya Samilyak <samilyakilya@gmail.com>
2023-06-20 14:01:53 +0300

    hw-hibernate

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
     public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Pirates of Caribbean sea");
+        movie.setDescription("This movie is about adventure");
+        movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
 
     }
 }
