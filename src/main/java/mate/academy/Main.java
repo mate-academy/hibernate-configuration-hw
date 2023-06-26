d811e2149c7aaf85a057e2700a53d79d448792be
Artem <cr7realartem@gmail.com>
2023-06-26 15:24:10 +0300

    done

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie itMovie = new Movie();
+        itMovie.setTitle("It");
+        Movie scaryMovie = new Movie();
+        scaryMovie.setTitle("scary movie");
+        movieService.add(itMovie);
+        movieService.add(scaryMovie);
+        movieService.get(itMovie.getId());
+        movieService.get(scaryMovie.getId());
     }
 }
