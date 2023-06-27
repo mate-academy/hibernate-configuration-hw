79d85c43b52168164bef793d5df7ef06a329d4dd
Taras Machyta <taras.machyta.sa@gmail.com>
2023-06-27 20:17:56 +0300

    Updates

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -7,7 +7,6 @@ import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 @Dao
@@ -42,7 +41,6 @@ public class MovieDaoImpl implements MovieDao {
     @Override
     public Optional<Movie> get(Long id) {
         Movie movie = null;
-
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             movie = session.get(Movie.class, id);
         } catch (Exception e) {
