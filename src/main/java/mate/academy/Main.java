a8d94b3aa409272be15741b4f28ade70bf1a45e6
Володимир <vova.malyarchuk2@gmail.com>
2023-07-04 15:41:43 +0300

    Implemented task 2

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,17 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService
+            = (MovieService) injector.getInstance(MovieService.class);
+
     public static void main(String[] args) {
 
+        movieService.add(new Movie("Lord of the rings", "Film about the rings"));
+        System.out.println(movieService.get(1L));
     }
 }
