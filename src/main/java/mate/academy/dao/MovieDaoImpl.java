789972adcc7a22e3fb4a2606fcb27bfb4d1b09c6
Serhii Dudko <nechupara@gmail.com>
2023-07-04 19:18:41 +0300

    implemented first version of hibernate config

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,51 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+    @Override
+    public Movie add(Movie movie) {
+
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(movie);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save movie " + movie.getTitle()
+                                              + " in DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        Movie movie = null;
+        try (Session session = sessionFactory.openSession()) {
+            movie = session.get(Movie.class, id);
+        } catch (HibernateException e) {
+            throw new DataProcessingException("Can't get movie with id: " + id, e);
+        }
+        return Optional.ofNullable(movie);
+    }
+}
