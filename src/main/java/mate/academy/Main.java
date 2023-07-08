cfa681fad799ded103f18c21c5ad026df3970172
DVArt <dvart@hotmail.com>
2023-07-09 01:59:51 +0300

    Structured

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,15 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService
+            = (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        //movieService.add(new Movie("FastAndFurious", "Racing"));
+        //System.out.println(movieService.get(1L));
     }
 }
