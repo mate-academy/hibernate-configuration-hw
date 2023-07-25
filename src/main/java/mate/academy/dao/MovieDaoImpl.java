1426eef4a0de1dc1b91f5339f559f74c935ea41d
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-07-25 18:14:51 +0300

    passed all tests

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.dao;
 
 import java.util.Optional;
+import mate.academy.exceptions.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
@@ -13,11 +14,23 @@ public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(movie);
-        transaction.commit();
-        session.close();
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save movie " + movie + " to DB");
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
         return movie;
     }
 
@@ -25,7 +38,14 @@ public class MovieDaoImpl implements MovieDao {
     public Optional<Movie> get(Long id) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         Session session = sessionFactory.openSession();
-        Movie movie = (Movie) session.get(Movie.class, id);
-        return Optional.of(movie);
+        try {
+            return Optional.ofNullable(session.get(Movie.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get movie with id " + id);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
