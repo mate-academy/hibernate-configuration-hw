4c2f109e2a73838926f424418e5178624b49c48f
AndriiBohun <poikka69@gmail.com>
2023-07-26 16:36:45 +0300

    C002_hibernate_hw

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,8 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
-        //1
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate");
+
+    public static void main(String[] args) {
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+
+        Movie lordOfTheRing1 = new Movie("Lord of the ring 1", "fantasy");
+        movieService.add(lordOfTheRing1);
+
+        System.out.printf(movieService.get(lordOfTheRing1.getId()).toString());
     }
 }
