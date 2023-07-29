e4f0dc11595c4b323e86f949b80f0b344cdf8b9d
Zakharchenko <zakharchenko.andrii@gmail.com>
2023-07-29 16:17:07 +0300

    hibernate-configuration-hw31-ready-to-check

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Terminator");
+        movie.setDescription("Movie about robot");
+        MovieService movieService = (MovieService) injector
+                .getInstance(MovieService.class);
+        System.out.println("Createt object - " + movie);
+        System.out.println("Added to DB - " + movieService.add(movie));
+        System.out.println("Get from DB - " + movieService.get(movie.getId()));
     }
 }
