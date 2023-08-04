f490d53b0b773badd0895c2c2cdd4ae1bff04ece
satori1111 <romakuchmii@gmail.com>
2023-08-04 18:33:54 +0300

    added solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieDao movieDao = (MovieDao) injector.getInstance(MovieDao.class);
+        Movie movie = new Movie();
+        movie.setDescription("dune");
+        movie.setTitle("new");
+        movieDao.add(movie);
+        movieDao.get(1L);
     }
 }
