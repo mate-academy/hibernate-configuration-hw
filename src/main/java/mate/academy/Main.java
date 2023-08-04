bf1bf553f8ced3db59de0d6a041b4842033598ac
ivan hromovyi <gromovoy1994@gmail.com>
2023-08-05 01:01:48 +0200

    implemented hibernate two dao methods

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        String title = "Night crawler";
+        String description = "terrifying photographer";
+        Movie movie = new Movie();
+        movie.setTitle(title);
+        movie.setDescription(description);
+        System.out.println(movieService.add(movie));
+        System.out.println(movieService.get(movie.getId()));
     }
 }
