b2d184ce25113e65d050e5ca06d298ce11fd94f8
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-08-07 18:28:17 +0300

    added exception and service

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,16 @@
 package mate.academy;
 
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.impl.MovieDaoImpl;
+import mate.academy.model.Movie;
+
 public class Main {
     public static void main(String[] args) {
-
+        Movie movie = new Movie();
+        movie.setTitle("JAVA JUNIOR DEVELOPER");
+        movie.setDescription("IN GENERAL ROLE: OLEG RAVLYK");
+        MovieDao movieDao = new MovieDaoImpl();
+        movieDao.add(movie);
+        System.out.println(movie.getId());
     }
 }
--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -1,19 +1,24 @@
 package mate.academy.dao.impl;
 
 import mate.academy.dao.MovieDao;
+import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.Optional;
 
+@Dao
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         Session session = sessionFactory.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
         session.save(movie);
+        transaction.commit();
         session.close();
         return movie;
     }
