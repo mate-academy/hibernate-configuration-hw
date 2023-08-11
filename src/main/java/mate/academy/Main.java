0578853e81a7b8a80200bfb7c379169254f1dc94
ZVIIR <denisarius2000s@gmail.com>
2023-08-11 13:35:55 +0300

    init commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) INJECTOR.getInstance(MovieService.class);
+        Movie movie = new Movie("The Shawshank Redemption",
+                "Top 1 IMDB movie");
+        Movie addedMovie = movieService.add(movie);
+        Movie receivedMovie = movieService.get(movie.getId());
+
+        System.out.println("Created movie: " + movie);
+        System.out.println("Added movie in DB: " + addedMovie);
+        System.out.println("Received movie from DB: " + receivedMovie);
     }
 }
