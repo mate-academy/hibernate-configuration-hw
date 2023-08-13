8b05ecafe67f3b11acfd89fd256b7703cb7d8b4e
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-08-13 22:02:28 +0300

    implemented movie operation with database v 1.0

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.services.MovieService;
 
+public class Main {
+    private static final Injector INJECTOR =
+            Injector.getInstance("mate.academy");
+    private static MovieService movieService =
+            (MovieService) INJECTOR.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("In a desert");
+        movie.setDescription("Funny no water life");
+
+        Movie result = movieService.add(movie);
+        System.out.println(result);
+        Movie movieFromDB = movieService.get(1L);
+        System.out.println(movieFromDB);
     }
 }
