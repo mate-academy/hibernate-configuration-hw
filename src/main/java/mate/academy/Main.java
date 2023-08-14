7636c270fbe151bd107156059dec20dba3ffa8fa
Ivan Fadieiev <ivanfadieiev@gmail.com>
2023-08-14 12:33:04 +0300

    implemented hibernate_configuration task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie oppenheimer = new Movie();
+        oppenheimer.setTitle("Oppenheimer");
+        movieService.add(oppenheimer);
+
+        Movie movieById = movieService.get(1L);
+        System.out.println(movieById);
     }
 }
+
---
+++ src/main/java/mate/academy/model/Movie.java
@@ -0,0 +1,36 @@
+package mate.academy.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
