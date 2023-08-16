98664d17cb7d7e430f2a3a8cb99f63983e9b5957
loranazarenko <loranazarenko26@gmail.com>
2023-08-16 18:33:28 +0300

    First commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,11 +1,13 @@
 package mate.academy;
 
+import mate.academy.lib.Inject;
 import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy.service");
+    @Inject
     private static final MovieService movieService
                              = (MovieService) injector.getInstance(MovieService.class);
 
@@ -15,7 +17,7 @@ public class Main {
         movie.setTitle("Terminator 5");
         movie.setDescription("Awesome movie");
         movieService.add(movie);
-        Movie movieFromDB = movieService.get(1L);
+        Movie movieFromDB = movieService.get(5L);
         System.out.println(movieFromDB);
     }
 }
--- src/main/java/mate/academy/service/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -1,6 +1,8 @@
 package mate.academy.service;
 
 import mate.academy.dao.MovieDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Inject;
 import mate.academy.lib.Injector;
 import mate.academy.lib.Service;
 import mate.academy.model.Movie;
@@ -8,6 +10,7 @@ import mate.academy.model.Movie;
 @Service
 public class MovieServiceImpl implements MovieService {
     private static final Injector injector = Injector.getInstance("mate.academy.dao");
+    @Inject
     private static final MovieDao movieDao = (MovieDao) injector.getInstance(MovieDao.class);
 
     @Override
