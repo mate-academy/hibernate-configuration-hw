71676903231612edc7f2a3aec36149b6c715328e
loranazarenko <loranazarenko26@gmail.com>
2023-08-16 18:13:16 +0300

    First commit

--- src/main/java/mate/academy/HibernateUtil.java
+++ src/main/java/mate/academy/HibernateUtil.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static final SessionFactory instance = initSessionFactory();
 
-    public static SessionFactory initSessionFactory() {
+    private static SessionFactory initSessionFactory() {
         return new Configuration().configure().buildSessionFactory();
     }
 
