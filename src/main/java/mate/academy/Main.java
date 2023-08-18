a268300bd58d681c8320c12e73cf91e417ea893d
Sergey Shestaka <sergey.shestaka@gmail.com>
2023-08-18 17:18:15 +0200

    1. hibernate dependencies added 2. hibernate.cfg.xml file created 3. HibernateUtil class created 4. Movie model created 5. MovieDao with implementation created 6. MovieService created

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,24 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector
+            = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Harley Davidson and the Marlboro Man");
+        movie.setDescription("Classic");
+
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        System.out.println("***Add new Movie***");
+        System.out.println(movieService.add(movie));
+
+        System.out.println("***Get Movie by id***");
+//        System.out.println(movieService.get(1L));
     }
 }
