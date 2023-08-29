a9ab4951320571e23d780f82fef6f102da20c127
Serhii Nosulia <sergeynosylya@gmail.com>
2023-08-30 01:09:50 +0300

    added fix

--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -7,16 +7,18 @@ import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
+        SessionFactory factory = HibernateUtil.getInstance();
         Session session = null;
         Transaction transaction = null;
         try {
-            session = HibernateUtil.getInstance().openSession();
+            session = factory.openSession();
             transaction = session.beginTransaction();
             session.save(movie);
             transaction.commit();
@@ -37,6 +39,8 @@ public class MovieDaoImpl implements MovieDao {
     public Optional<Movie> get(Long id) {
         try (Session session = HibernateUtil.getInstance().openSession()) {
             return Optional.ofNullable(session.get(Movie.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get movie by id: " + id, e);
         }
     }
 }
