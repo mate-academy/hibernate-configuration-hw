3de20db17adbf07ab33943c825d63df6168a99f0
Sergei Kudelin <chertterner686@gmail.com>
2023-08-30 09:04:21 +0300

    first commit

---
+++ src/main/java/mate/academy/dao/implementation/MovieDaoImpl.java
@@ -0,0 +1,48 @@
+package mate.academy.dao.implementation;
+
+import java.util.Optional;
+import mate.academy.dao.MovieDao;
+import mate.academy.exceptions.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Unable to save " + movie + " to DB", e.getCause());
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+                Session session = sessionFactory.openSession()) {
+            return Optional.ofNullable(session.get(Movie.class, id));
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get a movie by id " + id
+                    + ". There's no such movie", e);
+        }
+    }
+}
