80401b487ff7338ecadac3f57f53fcce832b95bd
ivanovalyub56 <140909880+ivanovalyub56@users.noreply.github.com>
2023-09-02 00:25:43 +0300

    Remove final

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -5,14 +5,14 @@ import mate.academy.models.Movie;
 import mate.academy.service.MovieService;
 
 public class Main {
-    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
-    private static final MovieService MOVIE_SERVICE = (MovieService) INJECTOR
+    private static Injector injector = Injector.getInstance("mate.academy");
+    private static MovieService movieService = (MovieService) injector
             .getInstance(MovieService.class);
 
     public static void main(String[] args) {
         Movie movie = new Movie();
         movie.setTitle("Sony 5");
         movie.setDescription("Livery");
-        MOVIE_SERVICE.add(movie);
+        movieService.add(movie);
     }
 }
