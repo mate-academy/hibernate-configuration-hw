3e5528968c386fce8a420e5f2664d74b1e170cd4
ivanovalyub56 <140909880+ivanovalyub56@users.noreply.github.com>
2023-09-01 22:40:30 +0300

    Solution of this task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.models.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+    private static final MovieService MOVIE_SERVICE = (MovieService) INJECTOR
+            .getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Sony 5");
+        movie.setDescription("Livery");
+        MOVIE_SERVICE.add(movie);
     }
 }
