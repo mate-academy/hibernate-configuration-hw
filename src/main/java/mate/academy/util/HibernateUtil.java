c767c4bebf8eb5ef7f986481d2cb0fda8ceed8fc
Dmytro Hanshyn <di.hanshyn@gmail.com>
2023-09-07 15:05:30 +0300

    Added MovieDao, MovieService

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -4,12 +4,17 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static final SessionFactory instance = initSessionFactory();
+    private static final SessionFactory INSTANCE = new Configuration()
+            .configure().buildSessionFactory();
+
+    private HibernateUtil(){
+    }
 
     private static SessionFactory initSessionFactory() {
         return new Configuration().configure().buildSessionFactory();
     }
+
     public static SessionFactory getSessionFactory() {
-        return instance;
+        return INSTANCE;
     }
 }
