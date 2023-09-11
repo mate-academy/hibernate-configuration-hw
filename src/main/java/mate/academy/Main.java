668aa1e0dbb2c0c6ad32c76a4c64ca086f2164f3
yurlis <yuri.lisovski@gmail.com>
2023-09-11 21:46:51 +0300

    First resolving stage done

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector INJECTOR_MAP = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) INJECTOR_MAP.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("Test");
+        movie.setDescription("Test description");
+
+        movieService.add(movie);
+
+        System.out.println(movieService.get(1L));
     }
 }
