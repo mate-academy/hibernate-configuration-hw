fe872a6409f39f634c3cfec0ec112a04fcbb2752
IhorPetrytskyi333 <mygit333@gmail.com>
2023-09-18 20:20:01 -0700

    hibernate_commit_3

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -7,6 +7,13 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static final SessionFactory SESSION_FACTORY = buildSessionFactory();
 
+    private HibernateUtil() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return SESSION_FACTORY;
+    }
+
     private static SessionFactory buildSessionFactory() {
         try {
             return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
@@ -14,14 +21,5 @@ public class HibernateUtil {
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
-    }
-
-    public static SessionFactory getSessionFactory() {
-        return SESSION_FACTORY;
-    }
-
-    public static void shutdown() {
-        // Close caches and connection pools
-        getSessionFactory().close();
     }
 }
