513afb28a56d8068faa5e138b2c9b605b6703ae9
Vershynin Kostia <bigvini@gmail.com>
2023-09-24 22:06:04 +0300

    realisation hibernate configuration

---
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -0,0 +1,18 @@
+package mate.academy.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory INSTANCE = initSessionFactory();
+
+    public static SessionFactory getSessionFactory() {
+        return INSTANCE;
+    }
+    private HibernateUtil() {
+    }
+
+    private static SessionFactory initSessionFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+}
