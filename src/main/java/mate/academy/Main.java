fe89a4bb32ee9892797cb58a6f28982011e011ea
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-09-25 14:38:12 +0300

    created n-tier architecture for Movie class

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie firstMovie = new Movie(
+                1L, "Inception", "It will make your brain work!");
+        movieService.add(firstMovie);
+        Movie secondMovie = new Movie(
+                2L, "Fast and Furious 10", "Recommend you to save few hours of your life)");
+        movieService.add(secondMovie);
+        System.out.println(movieService.get(2L));
 
     }
 }
