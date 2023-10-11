cfe24a436fcae424fd5c4c1a8bb45e3eebb81d63
Vitalii Voitseshuk <voitseshuk.v@gmail.com>
2023-10-11 23:22:53 +0300

    add Movie, MovieDao & MovieService implementation

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,44 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.service.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            session.persist(movie);
+            transaction.commit();
+        } catch (HibernateException e) {
+            throw new DataProcessingException("Can't save movie: " + movie, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Movie movie;
+        try (Session session = sessionFactory.openSession()) {
+            movie = session.get(Movie.class, id);
+        } catch (HibernateException e) {
+            throw new DataProcessingException("Can't get movie with id: " + id, e);
+        }
+        return Optional.ofNullable(movie);
+    }
+}
