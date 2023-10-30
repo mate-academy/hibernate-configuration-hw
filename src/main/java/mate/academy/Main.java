9b25ac8debc11167df9e706a1d6a01d65e686324
Taras Nedaskivskyi <nadashkivskyitv@gmail.com>
2023-10-30 20:33:10 +0200

    task solution - implemented add and get methods using 3-tier architecture and Hibernate framework

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        Movie movieFirst = new Movie();
+        movieFirst.setTitle("The kingdom of heaven");
+        movieFirst.setDescription("Interesting movie");
+
+        MovieService movieService = (MovieService) INJECTOR.getInstance(MovieService.class);
+
+        movieService.add(movieFirst);
+        System.out.println(movieFirst);
+
+        Movie movieFromDb = movieService.get(1L);
+        System.out.println(movieFromDb);
     }
 }
