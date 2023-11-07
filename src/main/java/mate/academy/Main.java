b8f2bae93734f93bfc1690cc7ad42bed2a147bd8
Victor Rudkovsky <vpcomtek@gmail.com>
2023-11-07 18:51:52 +0200

    hibernate-configuration-hw

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        final Injector injector = Injector.getInstance("mate.academy");
+        final MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movieToAdd = new Movie();
+        movieToAdd.setTitle("Title 1");
+        movieToAdd.setDescription("Description 1");
+
+        movieService.add(movieToAdd);
+        final Movie movieFromDb = movieService.get(1L);
+        System.out.println(movieFromDb);
 
     }
 }
