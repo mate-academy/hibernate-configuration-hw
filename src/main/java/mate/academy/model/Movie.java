724d09cf0cb94d4f75d443a257b3ef7496a48e82
Bohdan Bilko <rapsbodik@gmail.com>
2023-11-15 15:37:24 +0200

    fix checkstyle

--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -2,9 +2,9 @@ package mate.academy.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.GenerationType;
 
 @Entity
 @Table(name = "movies")
