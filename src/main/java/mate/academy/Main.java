80b33eaf03199a856bd3ae35224f368f0ed4c159
Nikita <kaptetos@gmail.com>
2023-11-17 11:52:19 +0200

    Main class containing the entry point for the Movie application. Initializes and runs the application

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+
 public class Main {
     public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
+        Movie movieToAdd = new Movie();
+        movieToAdd.setTitle("Inception");
+        Movie addedMovie = movieService.add(movieToAdd);
+        System.out.println("Added Movie: " + addedMovie);
+
+        Long movieIdToGet = addedMovie.getId();
+        Movie retrievedMovie = movieService.get(movieIdToGet);
+        System.out.println("Retrieved Movie: " + retrievedMovie);
     }
 }
