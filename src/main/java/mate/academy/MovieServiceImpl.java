39c00e10e31863c7323c99035a69253b802a893b
Nikita <kaptetos@gmail.com>
2023-11-17 11:51:27 +0200

    Implementation of the MovieService interface, providing logic for Movie entities.

---
+++ src/main/java/mate/academy/MovieServiceImpl.java
@@ -0,0 +1,21 @@
+package mate.academy;
+
+public class MovieServiceImpl implements MovieService {
+
+    private final MovieDao movieDao;
+
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.movieDao = movieDao;
+    }
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id).orElseThrow(() ->
+                new DataProcessingException("Movie with id " + id + " not found"));
+    }
+}
