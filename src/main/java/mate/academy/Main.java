538a5b66497b75b21f36d84d019922179754193d
yevhenreuta <r3utaa@gmail.com>
2023-11-25 15:03:13 +0200

    implemented movie dao and movie service

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final String MAIN_PACKAGE_NAME = "mate.academy";
+
+    public static void main(String[] args) {
+        Injector injector = Injector.getInstance(MAIN_PACKAGE_NAME);
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Titanic");
+        movie.setDescription("Bla bla bla");
+        System.out.println(movieService.add(movie));
+        System.out.println(movieService.get(1L));
     }
 }
