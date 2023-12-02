461be17e90d04046a7c7e76e8e3a57a4f80de403
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-12-02 11:52:15 +0200

    added solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    private static final MovieService movieService = (MovieService) injector
+            .getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie1 = new Movie();
+        movie1.setTitle("Game of Thrones");
+        movie1.setDescription("Winter is coming");
+        System.out.println(movieService.add(movie1));
+
+        System.out.println(movieService.get(movie1.getId()));
     }
 }
