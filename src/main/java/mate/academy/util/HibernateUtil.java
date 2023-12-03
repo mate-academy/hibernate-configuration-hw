ddcb7152983ba48b30c1efa630be92fa6cbfa8ef
Vasyl Palokha <xaski514@gmail.com>
2023-12-03 17:29:31 +0200

    HW-fix

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -6,12 +6,16 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static final SessionFactory instance = intiSessionFactory();
 
+    private HibernateUtil() {
+
+    }
+
     private static SessionFactory intiSessionFactory() {
         return new Configuration().configure().buildSessionFactory();
     }
 
     public static SessionFactory getSessionFactory() {
-        return instance;
+        return intiSessionFactory();
     }
 
 }
