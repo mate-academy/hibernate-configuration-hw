8f76e64064628b44d43775d3411827297778218d
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-06 22:31:04 +0200

    added changes to the class MovieDaoImpl

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,2 +1,41 @@
-package mate.academy.dao;public class MovieDaoImpl {
+package mate.academy.dao;
+
+import java.util.Optional;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+
+    private final SessionFactory sessionFactory;
+
+    public MovieDaoImpl() {
+        this.sessionFactory = HibernateUtil.getSessionFactory();
+    }
+
+    @Override
+    public Movie add(Movie movie) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+        } catch (Exception e) {
+            throw new DataProcessingException("Error adding movie to the database", e);
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            Movie movie = session.get(Movie.class, id);
+            return Optional.ofNullable(movie);
+        } catch (Exception e) {
+            throw new DataProcessingException("Error getting movie from the database", e);
+        }
+    }
 }
