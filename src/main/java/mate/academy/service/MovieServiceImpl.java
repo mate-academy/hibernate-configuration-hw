1eb54f7372d769703e8b5d173cd5c94cb39685e0
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-06 22:24:37 +0200

    added changes to the class MovieServiceImpl

--- src/main/java/mate/academy/service/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -1,2 +1,30 @@
-package mate.academy.service;public class MovieServiceImpl {
+package mate.academy.service;
+
+import mate.academy.dao.DataProcessingException;
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Inject;
+import mate.academy.model.Movie;
+
+public class MovieServiceImpl implements MovieService {
+
+    @Inject
+    private MovieDao movieDao;
+
+    @Override
+    public Movie add(Movie movie) {
+        try {
+            return movieDao.add(movie);
+        } catch (DataProcessingException e) {
+            throw new DataProcessingException("Error adding movie", e);
+        }
+    }
+
+    @Override
+    public Movie get(Long id) {
+        try {
+            return movieDao.get(id).orElse(null);
+        } catch (DataProcessingException e) {
+            throw new DataProcessingException("Error getting movie by id", e);
+        }
+    }
 }
