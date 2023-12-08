f6ba1861c9c71e8877e1d2dad675b63917f33034
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-08 23:59:12 +0200

    added changes to the class MovieDaoImp

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,12 +1,12 @@
 package mate.academy.dao;
 
 import java.util.Optional;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
