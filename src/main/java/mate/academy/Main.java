f8039ea2fccb121b1f57aecb1050d106fb7922b6
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-10 22:13:40 +0200

    added changes to the class Main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,11 +1,18 @@
 package mate.academy;
 
 import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
 
 public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        Injector injector = Injector.getInstance("mate.academy");
-        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        MovieService movieService = (MovieService) INJECTOR.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Terminator");
+        movie.setDescription("Good movie!!!");
+        movieService.add(movie);
+        System.out.println(movieService.get(1L));
     }
 }
