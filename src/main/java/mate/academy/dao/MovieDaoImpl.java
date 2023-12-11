6504c9738133a4a8dfb33580facd44912b34d610
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-11 20:14:09 +0200

    added changes to the class MovieDaoImpl

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -5,19 +5,16 @@ import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
-    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
     @Override
     public Movie add(Movie movie) {
         Session session = null;
         Transaction transaction = null;
         try {
-            session = sessionFactory.openSession();
+            session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
             session.save(movie);
             transaction.commit();
@@ -25,7 +22,7 @@ public class MovieDaoImpl implements MovieDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't add movie to the DB: " + movie);
+            throw new DataProcessingException("Can't add movie to DB", e);
         } finally {
             if (session != null) {
                 session.close();
@@ -36,17 +33,8 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public Optional<Movie> get(Long id) {
-        Session session = null;
-        Optional<Movie> movieOptional;
-        try {
-            session = sessionFactory.openSession();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return Optional.ofNullable(session.get(Movie.class, id));
-        } catch (Exception e) {
-            throw new DataProcessingException("Can't get movie with ID: " + id);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
     }
 }
