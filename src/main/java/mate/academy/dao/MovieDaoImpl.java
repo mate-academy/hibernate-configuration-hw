599f57a03d9cf939c5562a9e90155af1688673cd
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-11 15:17:19 +0200

    added changes to the class MovieDaoImpl

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -36,17 +36,11 @@ public class MovieDaoImpl implements MovieDao {
     @Override
     public Optional<Movie> get(Long id) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = null;
-        Optional<Movie> movieOptional;
-        try {
-            session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             return Optional.ofNullable(session.get(Movie.class, id));
         } catch (Exception e) {
             throw new DataProcessingException("Can't get movie with ID: " + id);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
     }
 }
+
