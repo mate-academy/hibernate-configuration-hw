e4fc4ac46dcffe269db98b7d54175d098705a68b
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-12 20:15:59 +0200

    added changes to the class Main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,8 +1,8 @@
 package mate.academy;
 
-import mate.academy.service.MovieService;
 import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
 public class Main {
     private static final Injector INJECTOR = Injector.getInstance("mate.academy");
