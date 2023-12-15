a27b3354a2d1d63f8c10e6be6f7a1ac116963f47
Bohdan Sosunovych <bohdan.sosunovych@gmail.com>
2023-12-15 16:08:01 +0200

    reached_configuration_of_Hibernate

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("Harry Hotter 1");
+        movie.setDescription("A film about the Philosopher's Stone");
+        movieService.add(movie);
+        System.out.println(movie + " was added");
+
+        Movie movieByIdOne = movieService.get(1L);
+        System.out.println("Movie with id 1 = " + movieByIdOne);
     }
 }
