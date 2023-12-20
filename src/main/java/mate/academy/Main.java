d99759e2920dca18afbe2adf71c276a3fbb6ef79
TkachukTara <zaz42988@gmail.com>
2023-12-20 13:01:16 +0200

    Hw done!

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,7 +6,8 @@ import mate.academy.service.MovieService;
 
 public class Main {
     public static void main(String[] args) {
-        MovieService movieService = (MovieService) Injector.getInstance("mate.academy").getInstance(MovieService.class);
+        MovieService movieService = (MovieService) Injector.getInstance("mate.academy")
+                .getInstance(MovieService.class);
 
         Movie theLastOfUs = new Movie();
         theLastOfUs.setTitle("The Last Of Us");
