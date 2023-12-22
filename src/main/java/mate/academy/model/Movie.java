164d337ff2f757c2f692d78ef5b22331e9edc198
arrem4eg <arem4eg@gmail.com>
2023-12-22 21:16:15 +0100

    fixed HW hibernate-configuration-hw

--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -2,8 +2,8 @@ package mate.academy.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
