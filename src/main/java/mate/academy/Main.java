a87e51a91e6a59e2e6e626adac959ab3a06f3e54
Kovaev <eugenkoval1010@gmail.com>
2023-12-26 17:44:48 +0200

    added hibernate configuration and Movie class

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,17 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie homeAlone = new Movie("Home Alone", "Christmas movie");
+        movieService.add(homeAlone);
+        movieService.get(homeAlone.getId());
+        movieService.get(95L);
     }
 }
