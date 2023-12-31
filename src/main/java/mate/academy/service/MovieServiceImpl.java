220fcecdb6fe0998dcf58c4a48fc0b59c2ada5a2
Piotr Kaliszuk <p_kaliszuk@tlen.pl>
2023-12-31 14:21:21 +0100

    solution

---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,29 @@
+package mate.academy.service;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+    private final MovieDao movieDao;
+
+    public MovieServiceImpl() {
+        this.movieDao = new MovieDaoImpl();
+    }
+
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.movieDao = movieDao;
+    }
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id).orElse(null);
+    }
+}
