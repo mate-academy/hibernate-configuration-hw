8ae802a0d2a4498e895a9ac770e5cc7f12258bae
Oleh Shvets <olehpost69@gmail.com>
2024-01-01 11:37:01 +0300

    hibernate-configuration-firstCommit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,28 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Riddikc");
+        movie.setDescription("Adventure movie");
+        movieService.add(movie);
+        System.out.println(movie);
+        Movie twelve = new Movie();
+        twelve.setTitle("Ocean's Twelve");
+        twelve.setDescription("Criminal");
+        movieService.add(twelve);
+        System.out.println(twelve);
+        System.out.println(movieService.get(movie.getId()));
+        System.out.println(movieService.get(twelve.getId()));
     }
 }
+
