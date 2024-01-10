61064e51671f6b9d9a9ccecfb78bca62afa89685
Nazar Kolesnikov <nazarkolesnikov536@gmail.com>
2024-01-10 23:57:24 +0200

    Initial commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector
+            .getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        Movie ghostsInVeniceMovie = new Movie("Ghosts in Venice", "detective");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(ghostsInVeniceMovie);
+        Movie anotherGhostsInVeniceMovie = movieService.get(1L);
+        System.out.println(anotherGhostsInVeniceMovie);
     }
 }
