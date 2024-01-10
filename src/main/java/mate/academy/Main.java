d4c09bc0ef3b558b26c85052c3c14808ecbe365a
LeonIllya <dd0733567@gmail.com>
2024-01-10 19:24:29 +0200

    first commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("Gentlemen");
+        movie.setDescription("Good movie");
+
+        movieService.add(movie);
+
+        Movie movieFromDB = movieService.get(movie.getId());
+        System.out.println(movieFromDB);
 
     }
 }
