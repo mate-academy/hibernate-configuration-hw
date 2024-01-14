b5a9790dc8dd32195c02b5fea4060732addefa06
Mykola Horianin <anyavic2379@gmail.com>
2024-01-14 16:42:58 +0200

    Added hibernate dependencies, created hibernate.cfg.xml file, created HibernateUtil classes, Movie model class, MovieDao interface, MovieDaoImpl class and custom unchecked DataProcessingException

---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,25 @@
+package mate.academy.service;
+
+import java.util.Optional;
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+    @Inject
+    private MovieDao movieDao = new MovieDaoImpl();
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        Optional<Movie> movie = movieDao.get(id);
+        return movie.get();
+    }
+}
