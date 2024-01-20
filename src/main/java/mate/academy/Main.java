88b6b0c285a6de313b6d9a502b86cddbde9567d5
Nonzeq <bgdn.kobylchak@gmail.com>
2024-01-20 18:37:10 +0200

    hibernate-cfg-hw

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        Movie movie1 = new Movie();
+        movie.setTitle("Test");
+        movie.setDescription("Test");
+        movie1.setTitle("Test 1");
+        movie1.setDescription("Test 1");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(movie1);
+        movieService.add(movie);
+        System.out.println(movieService.get(1L));
     }
 }
