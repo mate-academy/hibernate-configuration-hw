2ba291db9da4100c3e71141cbb1ac2fd7ebd6ec1
Anna Suprun <evgenevnaanna989@gmail.com>
2024-01-25 15:06:24 +0200

    created Movie, MovieDao, MovieService, exception, hibernate.cfg.xml, HibernateUtil, dependencies to pom.xml

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService service = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie1 = new Movie("Avatar", "American epic science fiction film");
+        Movie movie2 = new Movie("Lord of The Rings", "Epic fantasy adventure film");
+        service.add(movie1);
+        service.add(movie2);
+        Movie result = service.get(movie1.getId());
+        System.out.println(result);
     }
 }
