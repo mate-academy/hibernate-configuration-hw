417a24aa5025c13a0a05b7745486e659961dc189
artem <artemsa223@gmail.com>
2024-02-02 00:24:08 +0000

    impl dao&service

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+import mate.academy.service.MovieServiceImpl;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie1 = new Movie("Dota", "2");
+        Movie movie2 = new Movie("CS", "2");
+        movieService.add(movie1);
+        movieService.add(movie2);
+        Movie result = movieService.get(movie1.getId());
+        System.out.println(result);
     }
 }
