eb876a4a87c877cc4df267708586999aab8d32c0
gulini32 <gulini32@gmail.com>
2024-02-03 15:53:52 +0200

    resolve hw

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -5,6 +5,7 @@ import mate.academy.service.MovieService;
 
 public class Main {
     public static void main(String[] args) {
+
         Injector injector = Injector.getInstance("mate.academy");
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
     }
