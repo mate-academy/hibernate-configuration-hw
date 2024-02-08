b7337ef936bc1054e75b63f0e1104897ec193606
Bohdan <bohdan2704@gmail.com>
2024-02-08 19:53:23 +0200

    init commit

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,44 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(movie);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                System.out.println("Warning! Happened transaction rollback");
+            } else {
+                System.out.println("Warning! Transaction was not opened");
+            }
+        } finally {
+            if (session != null) {
+                System.out.println("Closing session");
+                session.close();
+            } else {
+                System.out.println("Warning. Session was not opened");
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Movie movie = session.get(Movie.class, id);
+            return Optional.ofNullable(movie);
+        }
+    }
+}
