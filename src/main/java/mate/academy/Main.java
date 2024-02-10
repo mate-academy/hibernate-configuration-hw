f088b5148ba11f6c02a0309e6336e3f78b7edc66
Mykola Malovanchuk <113469067+mykolazx3@users.noreply.github.com>
2024-02-10 22:44:16 +0200

    Update Main.java

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,10 @@
 package mate.academy;
 
+import mate.academy.lib.service.MovieServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
 
+        MovieServiceImpl movieService = new MovieServiceImpl();
     }
 }
