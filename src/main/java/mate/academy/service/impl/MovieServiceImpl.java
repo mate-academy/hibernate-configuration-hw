b45db6b2f018d0027c25d3e2aeba03fcb095f713
Oleh <mihailyakyb45@gmail.com>
2024-02-19 22:54:52 +0200

    done

---
+++ src/main/java/mate/academy/service/impl/MovieServiceImpl.java
@@ -0,0 +1,29 @@
+package mate.academy.service.impl;
+
+import java.util.Optional;
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+    @Inject
+    private MovieDao movieDao;
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        Optional<Movie> movieOptional = movieDao.get(id);
+        if (movieOptional.isPresent()) {
+            return movieOptional.get();
+        } else {
+            throw new RuntimeException("Can not find movie with id: " + id);
+        }
+    }
+}
