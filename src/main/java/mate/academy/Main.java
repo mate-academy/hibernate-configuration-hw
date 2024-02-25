ae0f5749d7836a8eac2764cfdf869c76082bc924
Kacper <kacperkukula97@gmail.com>
2024-02-25 15:56:42 +0100

    First Solution. Implemented Movie Dao and Service.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,30 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) INJECTOR.getInstance(MovieService.class);
+    private static final Movie TITATNIC_MOVIE = new Movie("Titanic",
+            "The Titanic was a British passenger liner that sank on its "
+                    + "maiden voyage in April 1912 after hitting an iceberg in the "
+                    + "North Atlantic.");
+    private static final Movie AVATAR_MOVIE = new Movie("Avatar",
+            "Avatar is a 2009 sci-fi film directed by James Cameron, "
+                    + "known for its groundbreaking 3D technology. It's set on the moon "
+                    + "Pandora and follows a human interacting with the Na'vi people.");
+
+    public static void main(String[] args) {
+        movieService.add(TITATNIC_MOVIE);
+        movieService.add(AVATAR_MOVIE);
+
+        Movie firstMovieFromDb = movieService.get(1L);
+        Movie secondMovieFromDb = movieService.get(2L);
+
+        System.out.println(firstMovieFromDb);
+        System.out.println(secondMovieFromDb);
     }
 }
