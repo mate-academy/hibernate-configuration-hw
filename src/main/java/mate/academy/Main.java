53366d3bcef6e57fb51efd733ce8491bd9d7c2ec
Farion Oleksandr <sashafarion@gmail.com>
2024-03-13 16:41:09 +0200

    another small step

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    public static void main(String[] args) {
+        MovieDao movieDao = (MovieDao) injector.getInstance(MovieDao.class);
+        Movie movie = new Movie();
+        movie.setTitle("Fast and Furious 9");
+        movie.setDescription("Great movie about families and cars.");
+        movieDao.add(movie);
+        Movie movie1 = movieDao.get(1L).get();
+        System.out.println(movie1);
     }
 }
---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,20 @@
+package mate.academy.service;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+
+    @Override
+    public Movie add(Movie movie) {
+        return null;
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return null;
+    }
+}
