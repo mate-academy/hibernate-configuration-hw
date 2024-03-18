7ef1e8f92e5f5d91f05b67865a74f0009c9d2a10
Artem Akymenko <akimenko.artem@gmail.com>
2024-03-18 20:52:13 +0200

    Refactored description.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -12,11 +12,11 @@ public class Main {
 
         Movie movie = new Movie();
         movie.setTitle("Nightmare on Big Mouse Street");
-        movie.setDescription("In a world where rodents have become humanity's greatest terror, " +
-                "a group of relentless mice sets out on a hunt for giant rats to save their " +
-                "neighborhood from the tyranny of terrifying rodents. In this fantasy-action " +
-                "comedy, the toothy heroes will battle for their territory in an epic clash " +
-                "between the eared and tailed!");
+        movie.setDescription("In a world where rodents have become humanity's greatest terror, "
+                + "a group of relentless mice sets out on a hunt for giant rats to save their "
+                + "neighborhood from the tyranny of terrifying rodents. In this fantasy-action "
+                + "comedy, the toothy heroes will battle for their territory in an epic clash "
+                + "between the eared and tailed!");
 
         System.out.println("Movie added: " + movieService.add(movie));
         System.out.println("Movie get by id 1: " + movieService.get(movie.getId()));
