cc51adf9fb75dcd57363ca8eaca2f64205746f09
bobik <oliferchukrivne@i.ua>
2024-03-18 12:03:11 +0200

    done2

--- src/main/java/mate/academy/service/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -3,8 +3,10 @@ package mate.academy.service;
 import mate.academy.dao.MovieDao;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
 import mate.academy.model.Movie;
 
+@Service
 public class MovieServiceImpl implements MovieService {
     @Inject
     private MovieDao movieDao;
