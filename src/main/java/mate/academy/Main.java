a6ab0e7c8e033583907aa9ee70717df6d2567e0b
Orest Sorokowey <osorokowey@gmail.com>
2024-03-20 11:00:55 +0200

    start

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 public class Main {
     public static void main(String[] args) {
-
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session mySession = sessionFactory.openSession();
+        Transaction transaction = mySession.beginTransaction();
+        Movie move = new Movie();
+        move.setTitle("mymoive");
+        mySession.save(move);
+        transaction.commit();
+        mySession.close();
     }
 }
---
+++ src/main/java/mate/academy/model/Movie.java
@@ -0,0 +1,28 @@
+package mate.academy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigInteger;
+
+@javax.persistence.Entity
+public class Movie {
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private BigInteger id;
+    private String title;
+    private String description;
+
+    public Movie(BigInteger id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+        public Movie() {
+        }
+
+    public void setTitle(String mymoive) {
+    }
+}
