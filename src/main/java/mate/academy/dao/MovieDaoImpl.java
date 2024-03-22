88eabd6b932c8331fe6df94817739abcec3193eb
orest40th <149381887+orest40th@users.noreply.github.com>
2024-03-22 13:26:31 +0200

    Update src/main/java/mate/academy/dao/MovieDaoImpl.java
    Co-authored-by: Kirill Koval <73041091+kerrrusha@users.noreply.github.com>

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -18,14 +18,15 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public Movie save(Movie movie) {
-        mySession = null;
+        Session session = null;
         Transaction transaction = null;
-
         try {
-            transaction = mySession.beginTransaction();
-            mySession.save(movie);
+            SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
             transaction.commit();
-
+            return movie;
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
