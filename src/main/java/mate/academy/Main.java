7753f524843c7fd9a68d15b351fc6362ea6f35e9
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-03-28 23:38:24 +0200

    feat:initial commit
    screated some scheme along lesson

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 public class Main {
     public static void main(String[] args) {
-
+        HibernateUtil hibernateUtil = new HibernateUtil();
+        SessionFactory sessionFactory = hibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Movie movie = new Movie();
+        movie.setTitle("Harry Potter 1");
+        session.save(movie);
+        transaction.commit();
+        session.close();
     }
 }
