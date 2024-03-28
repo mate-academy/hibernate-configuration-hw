fac77d0c736ff9b549fbe3bc92f9b8e62bf771fd
Valentyn Sharshon <woosay333@gmail.com>
2024-03-29 00:18:37 +0200

    added some test cases

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,25 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) INJECTOR.getInstance(MovieService.class);
+
+        Movie firstMovie = new Movie("Fast and furious", "Very interesting film");
+        Movie secondMovie = new Movie("Lord of the rings", "Amazing film");
+
+        movieService.add(firstMovie);
+        movieService.add(secondMovie);
+
+        Movie firstMovieFromDB = movieService.get(1L);
+        Movie secondMovieFromDB = movieService.get(2L);
+
+        System.out.println(firstMovieFromDB);
+        System.out.println(secondMovieFromDB);
     }
 }
