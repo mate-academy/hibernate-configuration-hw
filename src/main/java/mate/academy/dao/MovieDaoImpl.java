483c5a50f2022760963025e4ed4d56260e48e9f7
Oleksandr Samoylenko <ntskazhu1985@gmail.com>
2024-03-30 22:52:56 +0200

    Solution was added

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,51 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+    @Override
+    public Movie add(Movie movie) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException(String.format("Can't add movie to DB", movie), e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
+
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Optional optional = Optional.ofNullable(session.get(Movie.class, id));
+            return optional;
+        } catch (Exception e) {
+            throw new DataProcessingException(
+                    String.format("Cant get movie by id = %d from DB", id), e);
+        }
+    }
+}
