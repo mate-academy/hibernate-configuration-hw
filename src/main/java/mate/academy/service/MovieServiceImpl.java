e4e230c7f596ccc3afc2bd6d96a04758cde62e29
Hanna Ratushniak <ratushnyakhanna@gmail.com>
2024-03-31 17:46:06 +0300

    added nessesary classes and methods

---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,25 @@
+package mate.academy.service;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.exceptions.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.lib.Inject;
+import mate.academy.model.Movie;
+
+@Dao
+public class MovieServiceImpl implements MovieService {
+
+    @Inject
+    private MovieDao movieDao;
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id)
+                .orElseThrow(() -> new DataProcessingException("Can't find movie with id = " + id));
+    }
+}
