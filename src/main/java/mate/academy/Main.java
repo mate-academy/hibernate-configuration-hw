17d00d43d468547ba10b3b080ea8a2e0f610f595
Nazar Klimovych <rksvjdbx@gmail.com>
2024-03-31 12:44:35 +0300

    Added solution for task "hibernate configuration"

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        var movieService = (MovieService) injector.getInstance(MovieService.class);
+        var movie = new Movie("SelfieParty","Ukrainian comedy film");
+
+        Movie addedMovie = movieService.add(movie);
+        System.out.println("addedMovie = " + addedMovie);
+
+        Movie storedMovie = movieService.get(movie.getId());
+        System.out.println("storedMovie = " + storedMovie);
     }
 }
