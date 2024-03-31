21485eef2d5e5f019cd91f7b3d88bfd27f5dc194
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-03-31 16:57:34 +0300

    all test runs fine

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,25 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Movie MOVIE_FIRST = new Movie();
+    private static final Movie MOVIE_SECOND = new Movie();
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    static {
+        MOVIE_FIRST.setTitle("Mad Max");
+        MOVIE_FIRST.setDescription("Action");
+        MOVIE_SECOND.setTitle("MIB");
+        MOVIE_SECOND.setDescription("Comedy/Action");
+    }
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(MOVIE_SECOND);
+        System.out.println(movieService.add(MOVIE_FIRST));
+        System.out.println(movieService.get(1L));
     }
 }
