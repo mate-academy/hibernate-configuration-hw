7bea47b039496acf4a3fb15542dc460c2a6596c9
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-04-01 11:37:06 +0300

    1) hibernate-configuration task is done.

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,43 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    private Session session = null;
+    private Transaction transaction = null;
+
+    @Override
+    public Movie add(Movie movie) {
+        try {
+            SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(movie);
+            transaction.commit();
+            session.close();
+            return movie;
+        } catch (Exception e) {
+            transaction.rollback();
+            throw new DataProcessingException("Error adding entity to the database", e);
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(Movie.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("can't get an Entity from the DB", e);
+        }
+    }
+}
