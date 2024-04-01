925aade570ed7c75f1903614abd619bb983f3bcd
Slava_Moiseev <qualityengineerinkom@gmail.com>
2024-04-01 21:40:37 +0300

    push for char

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.lib.dao.MovieService;
+import mate.academy.model.Movie;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("Titan");
+        movie.setDescription("Cool movie");
+
+        Movie addMovie = movieService.add(movie);
+        System.out.println(addMovie);
     }
 }
