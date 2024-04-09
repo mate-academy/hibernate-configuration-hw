93a868710a76de5b684d6e9131dd974c382edcd0
trayvor <dalv23092005@gmail.com>
2024-04-10 01:50:13 +0200

    yo

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static Injector injector = Injector.getInstance("mate.academy");
+    private static MovieService movieService
+            = (MovieService) injector.getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("title");
+        movie.setDescription("description");
+
+        Movie addedMovie = movieService.add(movie);
+        Movie gottenMovie = movieService.get(1L);
+
+        System.out.println("Added: " + addedMovie + "\n" + "Gotten: " + gottenMovie);
     }
 }
