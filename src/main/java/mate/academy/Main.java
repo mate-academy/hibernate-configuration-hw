d48d52c6f11d60bf63d19b9a97bb3bd2a179687b
Sasha <sasha.panchyshun@gmail.com>
2024-04-10 19:43:47 +0300

    hibernate-configuration-hw-solution 1

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Inject;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    @Inject
+    private static MovieService movieService;
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Harry Potter ");
+        movie.setDescription("Cool");
+        movieService.add(movie);
+
+        movie.setTitle("Harry Potter 2");
+        movie.setDescription("Super Cool");
+        movieService.add(movie);
+
+        Movie movieFromDB = movieService.get(2L);
     }
 }
