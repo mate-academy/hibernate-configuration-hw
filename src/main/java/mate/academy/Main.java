df4bf897190579bb10504657e4f1fb4ac60209a7
Kinereiko <kinereiko@gmail.com>
2024-04-21 22:11:54 +0300

    Create Movie class, MovieDao and MovieService

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector =
+            Injector.getInstance("mate.academy.service");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Terminator 5");
+        movie.setDescription("Robot movie");
+
+        movieService.add(movie);
+        Movie movieFromDB = movieService.get(1L);
+        System.out.println(movieFromDB);
     }
 }
