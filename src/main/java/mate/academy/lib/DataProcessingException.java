272e1695cbaf3bf7c1ac09b85725da7b33bae08e
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-04-24 15:12:20 +0300

    solve the task

---
+++ src/main/java/mate/academy/lib/DataProcessingException.java
@@ -0,0 +1,9 @@
+package mate.academy.lib;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException() {
+    }
+
+    public DataProcessingException(String message, Exception e) {
+    }
+}
