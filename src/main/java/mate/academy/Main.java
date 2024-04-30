40ca6426b0a52822526a805b54b56b740cac5605
nastasiiaKulinich <kussik2004@gmail.com>
2024-04-30 14:30:17 +0300

    implemented MovieService

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Sherlock Holmes");
+        movie.setDescription("A very interesting series, you can watch it forever. "
+                + "This is a timeless classic))");
+        Movie movieAddedToDB = movieService.add(movie);
+        System.out.println(movieAddedToDB);
+        Movie movieFromDB = movieService.get(movieAddedToDB.getId());
+        System.out.println(movieFromDB);
     }
 }
