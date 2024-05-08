7083bc0ac577fed6aa826dc3ba884f15a03fe484
Vlad <mazafackery@gmail.com>
2024-05-08 18:28:14 +0300

    commit with Main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setId(1L);
+        movie.setTitle("Taxi");
+        movie.setDescription("Awesome film");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        System.out.println(movieService.add(movie));
+        System.out.println(movieService.get(1L));
     }
 }
