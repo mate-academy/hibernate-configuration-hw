fcc68100b027836fe094ee7abb655b5147b66119
Dima <dmmhnk23@gmail.com>
2024-05-09 12:53:53 +0300

    finished task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,25 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+import mate.academy.service.MovieServiceImpl;
 
+public class Main {
+    private static final Injector injector =
+            Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+        Movie movie1 = new Movie();
+        movie1.setTitle("Harry Potter");
+        movie1.setDescription("Movie about boy...");
+        movieService.add(movie1);
+        Movie movie2 = new Movie();
+        movie2.setTitle("Hot Wheels");
+        movie2.setDescription("Movie about cars.");
+        movieService.add(movie2);
+        System.out.println(movieService.get(1L));
     }
 }
