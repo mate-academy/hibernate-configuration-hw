97f72dd67ad3ef58f4ba8e04ac166baca101ac4a
kvjohnny <kvjohnny.van@gmail.com>
2024-05-31 16:25:45 +0300

    created and fixed classes in hibernate-configuration task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,24 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieDao movieDao = (MovieDao) injector.getInstance(MovieDao.class);
+
+        Movie firstMovie = new Movie("Mad Max", "action movie");
+        System.out.println(movieDao.add(firstMovie));
+
+        Movie secondMovie = new Movie("Cinderella", "fairy tale");
+        System.out.println(movieDao.add(secondMovie));
+
+        Movie thirdMovie = new Movie("The Lion King", "cartoon");
+        System.out.println(movieDao.add(thirdMovie));
+
+        System.out.println(movieDao.get(2L));
     }
 }
