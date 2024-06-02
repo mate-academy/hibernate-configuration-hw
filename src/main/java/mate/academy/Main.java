8bfe91b6f8e2c3be8d3b59606ac0d903bf899fe1
Artem Klishch <artemklishch@gmail.com>
2024-06-02 13:23:56 +0300

    feat: should make logic to save movues data
    BREAKING CHANGES:
    
    *Made Hibernate util class
    *Made services and implementations for adding movy and fetchng movie by id
    
    Task: https://github.com/mate-academy/hibernate-configuration-hw

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.sevice.MovieService;
 
+public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) INJECTOR.getInstance(MovieService.class);
+        Movie movie = new Movie("Book of Jungles", "This is a book about jungles");
+        movieService.add(movie);
+
+        Movie movieById = movieService.get(1L);
+        System.out.println(movieById);
     }
 }
