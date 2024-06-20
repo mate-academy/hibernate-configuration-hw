e024b4e1c76af24d0a506d3eea853eed7cdb2542
Nazar <nazarmo13@gmail.com>
2024-06-20 19:09:36 +0300

    configured Hibernate

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("bleach");
+        movie.setDescription("some description");
+
+        movieService.add(movie);
+        System.out.println(movieService.get(1L));
     }
 }
