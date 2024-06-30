a364eaca034d3a6ed7522980729a02d90c7ea60a
Anton <antchaycka@gmail.com>
2024-06-30 19:02:06 +0200

    Finished task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,17 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie m1 = new Movie("Thing", "Horror movie");
+        Movie movie = movieService.add(m1);
+        Movie movie1 = movieService.get(1L);
+        System.out.println(movie1);
     }
 }
