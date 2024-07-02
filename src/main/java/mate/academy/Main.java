c9e7631c2fe4f5c44e00d6891036c6a565225cea
Banderan <bartoszwojcik2004@gmail.com>
2024-07-02 18:35:30 +0200

    mvn clean package + solve task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+import mate.academy.service.MovieServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
+        MovieDao movieDao = new MovieDaoImpl();
+        MovieService movieService = new MovieServiceImpl(movieDao);
+        Movie movie = new Movie();
+        movie.setTitle("new Movie");
+        movie.setDescription("exciting movie");
 
+        Movie add = movieService.add(movie);
+
+        Movie get = movieService.get(1L);
+        System.out.println(add);
+        System.out.println(get);
     }
 }
