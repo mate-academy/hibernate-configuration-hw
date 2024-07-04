004b4627c68f0931660d39c72c251e5a293c0506
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2024-07-04 16:44:38 +0300

    initial commit

---
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,17 @@
+package mate.academy.dao.impl;
+
+import java.util.Optional;
+import mate.academy.dao.MovieDao;
+import mate.academy.model.Movie;
+
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        return null;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        return Optional.empty();
+    }
+}
