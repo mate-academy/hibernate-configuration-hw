665b4861a462046328a091c341f7fed3229904e9
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-07-09 10:09:46 +0300

    HibernateUtil.java

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -6,6 +6,10 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
+    private HibernateUtil() {
+        // Prevent instantiation
+    }
+
     private static SessionFactory buildSessionFactory() {
         try {
             return new Configuration().configure().buildSessionFactory();
@@ -16,8 +20,5 @@ public class HibernateUtil {
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
-    }
-
-    private HibernateUtil() {
     }
 }
