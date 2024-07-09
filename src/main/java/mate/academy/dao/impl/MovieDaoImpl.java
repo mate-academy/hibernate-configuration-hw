e2bd2ccbdf61a01704fcadd06ddec0aeed154386
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-07-09 10:03:09 +0300

    MovieDaoImpl.java

--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.dao.impl;
 
+import java.util.Optional;
 import mate.academy.dao.MovieDao;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
@@ -7,7 +8,6 @@ import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import java.util.Optional;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
