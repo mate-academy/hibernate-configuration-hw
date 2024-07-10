8de1b9ff647ac0adb822c7652f39df8b824dc44d
Oleksii <sukhenko.oleksii@gmail.com>
2024-07-10 22:11:40 +0300

    first commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+import mate.academy.service.MovieServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
+        final MovieService movieService = new MovieServiceImpl();
 
+        Movie movie = new Movie();
+        movie.setTitle("The Hitchhiker's Guide to the Galaxy");
+        movie.setDescription("Knowing where one's towel is.");
+
+        movieService.add(movie);
+        Movie movieFromDB = movieService.get(1L);
+        System.out.println(movieFromDB);
     }
 }
