a0899f5bf789e53eab5082d952039686a61f9a0f
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-07-17 22:31:42 +0300

    hibernate-configuration-hw solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy.dao");
+
+    public static void main(String[] args) {
+        MovieDao movieDao = (MovieDao) injector.getInstance(MovieDao.class);
+
+        Movie movie = new Movie("Something special","something Ultra Very Special Pro Max");
+        movieDao.add(movie);
+
+        Movie getMovie = movieDao.get(1L).orElseThrow();
+        System.out.println(getMovie.getTitle() + " " + getMovie.getDescription());
     }
 }
---
+++ src/main/java/mate/academy/exception/DataProcessingException.java
@@ -0,0 +1,11 @@
+package mate.academy.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public DataProcessingException(String errorMessage, Throwable err) {
+        super(errorMessage, err);
+    }
+}
