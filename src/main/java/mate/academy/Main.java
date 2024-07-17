c02b6d25f8c3c69add0a2951c6688460930331b5
BelitskyiAlexandr <vilisapett@gmail.com>
2024-07-17 03:12:02 +0300

    new commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,12 +6,6 @@ import mate.academy.service.MovieService;
 
 public class Main {
     public static void main(String[] args) {
-        MovieService movieService = (MovieService) Injector.getInstance(
-                String.valueOf(MovieService.class));
-        Movie movie = new Movie();
-        movie.setTitle("movie");
 
-        movieService.add(movie);
-        movieService.get(1L);
     }
 }
