3142dfb2c67f3088b4b6b6af19789f9ed3f00d7c
citruscaviar <citruscaviar96@gmail.com>
2024-08-05 13:51:58 +0300

    completed the task 2

--- src/main/java/mate/academy/lib/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/lib/dao/MovieDaoImpl.java
@@ -12,8 +12,10 @@ import org.hibernate.Transaction;
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
+        Session session = null;
         Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
             session.save(movie);
             transaction.commit();
@@ -23,6 +25,10 @@ public class MovieDaoImpl implements MovieDao {
                 transaction.rollback();
             }
             throw new DataProcessingException("Couldn't add movie " + movie, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 
