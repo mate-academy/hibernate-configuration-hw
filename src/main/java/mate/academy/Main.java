fc07e49f1e60c86e8f810a4c7199ef68989347c9
citruscaviar <citruscaviar96@gmail.com>
2024-08-05 20:01:07 +0300

    needs to be fixed

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,16 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie1 = new Movie("Harry Potter","blahblah");
+        movieService.add(movie1);
+        movieService.get(1L);
     }
 }
