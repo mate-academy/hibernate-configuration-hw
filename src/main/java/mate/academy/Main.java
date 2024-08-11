b0101a364101b3f6ff8494a074ba0caccf9fd6b1
Serhii Kopervas <sergkopervas@gmail.com>
2024-08-11 22:11:47 +0200

    Version 1.0.1. First commit.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.model.Movie;
+
 public class Main {
     public static void main(String[] args) {
+        MovieDaoImpl movieDao = new MovieDaoImpl();
+        Movie movie = new Movie();
+        movie.setTitle("Pulp fiction");
+        movie.setDecription("Pulp Fiction is a 1994 American independent crime film "
+                + "written and directed by Quentin Tarantino "
+                + "from a story he conceived with Roger Avary.");
 
+        movieDao.add(movie);
+        System.out.println(movieDao.get(1L));
     }
 }
--- src/main/java/mate/academy/service/Service.java
+++ src/main/java/mate/academy/service/Service.java
@@ -1,4 +1,4 @@
-package mate.academy.lib;
+package mate.academy.service;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
