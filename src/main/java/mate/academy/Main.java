cba48ab8dfb4e36386d72a31c934d2e39b4356ec
Serhii Kopervas <sergkopervas@gmail.com>
2024-08-12 21:25:53 +0200

    Version 1.0.3. Error checked.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -4,19 +4,22 @@ import mate.academy.dao.MovieDao;
 import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         Injector injector = Injector.getInstance("mate.academy");
 
-        Movie movie = (Movie) injector.getInstance(MovieDao.class);
+        Movie movie = new Movie();
         movie.setTitle("Pulp fiction");
         movie.setDecription("Pulp Fiction is a 1994 American independent crime film "
                 + "written and directed by Quentin Tarantino "
                 + "from a story he conceived with Roger Avary.");
 
-        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        MovieService movieService = (MovieServiceImpl) injector.getInstance(MovieService.class);
         movieService.add(movie);
+
+        System.out.println(movieService.get(1L));
 
     }
 }
--- src/main/java/mate/academy/service/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -1,6 +1,8 @@
 package mate.academy.service;
 
 import java.util.Optional;
+
+import mate.academy.dao.MovieDao;
 import mate.academy.dao.MovieDaoImpl;
 import mate.academy.lib.Inject;
 import mate.academy.model.Movie;
@@ -8,7 +10,7 @@ import mate.academy.model.Movie;
 @Service
 public class MovieServiceImpl implements MovieService {
     @Inject
-    private MovieDaoImpl movieDao;
+    private MovieDao movieDao;
 
     @Override
     public Movie add(Movie movie) {
