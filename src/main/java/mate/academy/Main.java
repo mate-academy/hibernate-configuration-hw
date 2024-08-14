98db0c4b9764c8ebee6552655d9d9c0cbe7bbd35
Serhii Kopervas <sergkopervas@gmail.com>
2024-08-14 22:27:38 +0200

    Version 1.0.6. Added new code to Main class.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,5 +1,7 @@
 package mate.academy;
 
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
 import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
@@ -18,7 +20,13 @@ public class Main {
         MovieService movieService = (MovieServiceImpl) injector.getInstance(MovieService.class);
         movieService.add(movie);
 
-        System.out.println(movieService.get(1L));
+        Movie movie1 = new Movie();
+        movie1.setTitle("2001: A Space Odyssey ");
+        movie1.setDecription("2001: A Space Odyssey is a 1968 epic science fiction film "
+                + "produced and directed by Stanley Kubrick");
+        MovieDao movieDao = new MovieDaoImpl();
+        movieDao.add(movie1);
+        System.out.println(movieService.get(1L) + "/n" + movieService.get(2L));
 
     }
 }
