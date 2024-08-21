56583feb84bb28948d5f9aafc673749d60cfe8bf
foxstudent106239 <mykhailo.kornukh@gmail.com>
2024-08-21 22:20:39 +0300

    main 2

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,11 +1,5 @@
 package mate.academy;
 
-import mate.academy.dao.MovieDao;
-import mate.academy.dao.MovieDaoImpl;
-import mate.academy.domain.Movie;
-import mate.academy.service.MovieService;
-import mate.academy.service.MovieServiceImpl;
-
 public class Main {
     public static void main(String[] args) {
 
