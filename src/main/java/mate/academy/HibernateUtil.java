b27686701c35d8d2b08e8bc9b2753456c43d5ec0
foxstudent106239 <mykhailo.kornukh@gmail.com>
2024-08-21 23:02:59 +0300

    hibernate.cfg.xml 333

--- src/main/java/mate/academy/HibernateUtil.java
+++ src/main/java/mate/academy/HibernateUtil.java
@@ -12,7 +12,8 @@ public class HibernateUtil {
 
     private static SessionFactory initSessionFactory() {
         try {
-            return new Configuration().configure().buildSessionFactory();
+            return new Configuration().configure("src/main/resources/"
+                    + "hibernate.cfg.xml").buildSessionFactory();
         } catch (Exception e) {
             throw new RuntimeException("Can't initialize Hibernate SessionFactory", e);
         }
