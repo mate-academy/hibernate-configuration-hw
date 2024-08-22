9ec5ffe9e3b82ecc43d1b76d63f96254e513d723
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2024-08-22 18:52:10 +0200

    added dependencies: mysql-connector-java and hibernate-core created hibernate.cfg.xml created HibernateUtil created entity Movie created MovieDao/MovieService and implemented created custom DataProcessingException tested all methods from service layer in main class

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,25 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.entity.Movie;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Injector;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    @Inject
+    public static void main(String[] args) {
+        Movie first = new Movie();
+        first.setTitle("First Movie");
+        first.setDescription("Movie with id 1");
+        Movie second = new Movie();
+        second.setTitle("Second Movie");
+        second.setDescription("Movie with id 2");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        System.out.println(movieService.add(first));
+        System.out.println(movieService.add(second));
+        System.out.println(movieService.get(1L));
+        System.out.println(movieService.get(2L));
     }
 }
---
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,48 @@
+package mate.academy.dao.impl;
+
+import java.util.Optional;
+import mate.academy.dao.MovieDao;
+import mate.academy.entity.Movie;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(movie);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Transaction was rollback", e);
+        } finally {
+            if (transaction != null) {
+                session.close();
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        Movie movie;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            movie = session.get(Movie.class, id);
+        } catch (RuntimeException e) {
+            throw new DataProcessingException("There`s no way to get Movie by the id: " + id, e);
+        }
+        return Optional.ofNullable(movie);
+    }
+}
