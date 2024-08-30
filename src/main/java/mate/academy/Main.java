0d76c763e2c0b51e292cb47a0c4941626df82b98
OD <sdenisenko055@gmail.com>
2024-08-30 15:57:22 +0300

    result

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,24 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Inject;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy.dao");
+
+    public static void main(String[] args) {
+        Movie movie1 = new Movie();
+        movie1.setTitle("movie1");
+        movie1.setDescription("This is movie #1");
+        Movie movie2 = new Movie();
+        movie2.setTitle("movie2");
+        movie2.setDescription("This is movie #2");
+        @Inject
+        MovieService movieService = null;
+        movieService.add(movie1);
+        movieService.add(movie2);
+        System.out.println(movieService.get(2L));
     }
 }
