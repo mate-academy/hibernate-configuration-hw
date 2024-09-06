4e0adbf8c6c0a8b6fa9aabb335012b564298f8ff
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-09-06 17:12:22 +0300

    Created an instance of MovieService using injector and test all methods from MovieService

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie firstMovie = new Movie("Modern Java", "Best practises of coding");
+        Movie secondMovie = new Movie("Harry Potter and the Philosopher's Stone",
+                "It follows Harry Potter, a young wizard who discovers his magical heritage "
+                        + "on his eleventh birthday when he receives a letter of acceptance to "
+                        + "Hogwarts School of Witchcraft and Wizardry.");
+        Movie thirdMovie = new Movie("The Little Prince",
+                "The story follows a young prince who visits various planets, "
+                        + "including Earth, and addresses themes of loneliness, friendship, love, "
+                        + "and loss.");
+        movieService.add(firstMovie);
+        movieService.add(secondMovie);
+        movieService.add(thirdMovie);
+        movieService.get(2L);
     }
 }
