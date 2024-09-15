254a9ac66249fd3b2b85570529e26a89aebde83c
flamehost <flamehost@syneforge.com>
2024-09-15 11:27:45 +0300

    Configuration implementation

---
+++ src/main/java/mate/academy/service/impl/MovieServiceImpl.java
@@ -0,0 +1,29 @@
+package mate.academy.service.impl;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
+import javax.inject.Inject;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+    private final MovieDao movieDao;
+
+    @Inject
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.movieDao = movieDao;
+    }
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id).orElseThrow(() ->
+                new RuntimeException("Movie with id " + id + " not found"));
+    }
+}
