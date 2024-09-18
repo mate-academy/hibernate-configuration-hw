13be8a3c03fabcdcd319b9913c4d4d048834bf5a
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2024-09-18 17:42:11 +0200

    Ninth solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+import mate.academy.service.MovieServiceImpl;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        System.out.println("");
+        Movie terminator = new Movie();
+        terminator.setTitle("Terminator");
+        terminator.setDescription("Sci-fi");
+        MovieDao movieDao = (MovieDao) injector.getInstance(MovieDao.class);
+        MovieService movieService = new MovieServiceImpl(movieDao);
+        movieService.add(terminator);
+        movieService.get(1L);
     }
 }
