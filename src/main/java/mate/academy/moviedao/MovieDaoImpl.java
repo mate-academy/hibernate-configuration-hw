ae083aa89ce26a28b1aa02dd5217b1a4a759b4bc
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-09-23 16:16:19 +0300

    Implemented methods get/add

--- src/main/java/mate/academy/moviedao/MovieDaoImpl.java
+++ src/main/java/mate/academy/moviedao/MovieDaoImpl.java
@@ -1,17 +1,30 @@
 package mate.academy.moviedao;
 
 import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.Optional;
 
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
-        return null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(movie);
+        transaction.commit();
+        session.close();
+        return movie;
     }
 
     @Override
     public Optional<Movie> getById(Long id) {
-        return Optional.empty();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Movie movie = session.get(Movie.class, id);
+        return Optional.of(movie);
     }
 }
