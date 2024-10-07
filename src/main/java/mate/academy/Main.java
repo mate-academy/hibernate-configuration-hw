7f1bf94a98ed02d2e9034018b57a868ef0db532b
Oleksandr Prots <sashaproc2289@gmail.com>
2024-10-07 18:50:41 +0300

    hibernate-configuration-hw

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        MovieService movieService = (MovieService) Injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Harry Potter");
+        movie.setDescription("Nice Movie");
 
+        Movie saveMovie = movieService.add(movie);
+        Movie getMovie = movieService.get(1L);
+        System.out.println(getMovie);
     }
 }
---
+++ src/main/java/mate/academy/model/Movie.java
@@ -0,0 +1,42 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import mate.academy.lib.Inject;
+
+@Entity
+@Inject
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
