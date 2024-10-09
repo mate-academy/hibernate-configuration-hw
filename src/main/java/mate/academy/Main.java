6d4ecdbe15024fb92b048ef3004c063fb59b90c2
Mikhailo <88093373+MukhailoVorobets@users.noreply.github.com>
2024-10-09 18:16:45 +0300

    hibernate-configuration-solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        MovieService movieService = (MovieService) Injector.getInstance("mate.academy");
 
+        Movie movie = new Movie();
+        movie.setTitle("Terminator9");
+        movie.setDescription("Some description!");
+        movieService.add(movie);
+        Movie movieFromDb = movieService.get(2L);
+        System.out.println(movieFromDb);
     }
 }
