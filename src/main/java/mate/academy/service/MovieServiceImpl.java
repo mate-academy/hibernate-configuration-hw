6881d913ecee66be8de49656a664c3de5fb248c1
Xoteki <xotekijke@gmail.com>
2024-10-22 10:45:33 +0300

    solution added

---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,25 @@
+package mate.academy.service;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+    private final MovieDao movieDao;
+
+    public MovieServiceImpl() {
+        this.movieDao = new MovieDaoImpl();
+    }
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id).orElseThrow(() -> new RuntimeException("Movie not found"));
+    }
+}
