c5518868fbd8f0d6df63b4aa46d1008deb278c18
Oleksii Deineka <deineka219@gmail.com>
2024-11-01 21:13:41 +0200

    The task was written according to the requirements

---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,24 @@
+package mate.academy.service;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.model.Movie;
+
+public class MovieServiceImpl implements MovieService {
+    private final MovieDao movieDao;
+
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.movieDao = movieDao;
+    }
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id).orElseThrow(() ->
+                new DataProcessingException("Movie with id " + id + " not found", null));
+    }
+}
