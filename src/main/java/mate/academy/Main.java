ed843ee69502d139697625dd6a8fc2489bca92dc
SuperKate13 <tophours13@gmail.com>
2024-11-08 20:17:37 +0200

    refactor code

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -11,9 +11,6 @@ public class Main {
     private static final Injector INJECTOR = Injector.getInstance("mate.academy");
 
     public static void main(String[] args) {
-        MovieDao dao = (MovieDaoImpl) INJECTOR.getInstance(MovieDaoImpl.class);
-        MovieService service = (MovieServiceImpl) INJECTOR.getInstance(MovieServiceImpl.class);
-
         Movie movie1 = new Movie();
         movie1.setTitle("Terminator 2");
         movie1.setDescription("Best movie!!");
@@ -21,7 +18,8 @@ public class Main {
         Movie movie2 = new Movie();
         movie2.setTitle("LOTR 1");
         movie2.setDescription("ALSO Best movie!!");
-
+        MovieDao dao = (MovieDaoImpl) INJECTOR.getInstance(MovieDaoImpl.class);
+        MovieService service = (MovieServiceImpl) INJECTOR.getInstance(MovieServiceImpl.class);
         service.add(movie1);
         service.add(movie2);
 
