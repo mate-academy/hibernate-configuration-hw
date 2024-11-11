56f95b70823bb67018a69c6fd67accffb3767ec4
Julianevinska <Julianevinska@gmail.com>
2024-11-11 21:55:37 +0100

    hibernate configuration

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+import mate.academy.service.MovieServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
+        MovieService movieService = new MovieServiceImpl(new MovieDaoImpl());
 
+        Movie movie = new Movie();
+        movie.setTitle("Inception");
+        movie.setDescription("A sci-fi thriller");
+
+        movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
     }
 }
---
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -0,0 +1,26 @@
+package mate.academy.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory instance = initSessionFactory();
+
+    private static SessionFactory initSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return instance;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
