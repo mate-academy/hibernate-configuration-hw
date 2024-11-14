7ce03d270c8f8a973f47493e684aef0a358f5677
fwhoami <clahfclns@gmail.com>
2024-11-14 14:52:57 +0200

    Solution for hibernate-configuration

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,37 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) throws Exception {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("No movie has been added", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        return Optional.empty();
+    }
+}
