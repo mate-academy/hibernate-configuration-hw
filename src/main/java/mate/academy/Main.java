afc8829e3288ea3f72e1076ce4ec91371b02e675
clo3eX <pansydaisy83@gmail.com>
2024-11-15 04:24:24 +0100

    fixes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) INJECTOR.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("TITLE");
+        movie.setDescription("DESCRIPTION");
+        movieService.add(movie);
+        Movie movieFromDb = movieService.get(1L);
+        System.out.println(movieFromDb);
     }
 }
