bacbb96487462e2a6533447594d9283ed3f53210
Serhiy <sergie3007@gmail.com>
2024-11-17 02:08:37 +0200

    implement interaction movie entity with database

---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,23 @@
+package mate.academy.service;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+    @Inject
+    private MovieDao dao;
+
+    @Override
+    public Movie add(Movie movie) {
+        return dao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return dao.get(id).orElseThrow(()
+                -> new RuntimeException("Cannot get movie by id: " + id));
+    }
+}
