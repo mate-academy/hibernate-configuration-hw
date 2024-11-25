6ecf8142d65b196f70a8f76f32c56980201e534c
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-11-25 14:38:36 +0100

    implemented base logic

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,50 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import util.DataProcessingException;
+import util.HibernateUtil;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+    @Override
+    public Movie add(Movie movie) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(movie);
+            transaction.commit();
+            return movie;
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Error while adding object:" + movie.toString(), e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            return Optional.ofNullable(session.get(Movie.class, id));
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
