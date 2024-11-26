379fb1f7152d271c03a538dac81210135b82db97
Mateusz Seler <mateusz.seler@gmail.com>
2024-11-26 10:42:13 +0100

    first version

---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,37 @@
+package mate.academy.service;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+    private final MovieDao moveDao;
+
+    public MovieServiceImpl(MovieDao moveDao) {
+        this.moveDao = moveDao;
+    }
+
+    @Override
+    public Movie add(Movie movie) {
+        return moveDao.add(movie);
+    }
+
+    @Override
+    public Boolean update(Movie movie) {
+        Movie currentMovie = get(movie.getId());
+        moveDao.update(movie);
+        return currentMovie.equals(get(movie.getId()));
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return moveDao.get(id)
+                .orElseThrow(() -> new RuntimeException("there is no object with id: " + id));
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return moveDao.delete(id);
+    }
+}
