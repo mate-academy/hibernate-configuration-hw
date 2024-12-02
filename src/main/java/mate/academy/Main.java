d0ff7159285aa19cb28a475dce94bffdd6369573
d.nosenko <daniil.nosenkoo@gmail.com>
2024-12-03 00:06:29 +0100

    fix

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,23 @@
 package mate.academy;
 
-import mate.academy.dao.MovieDao;
 import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
-import mate.academy.service.MovieServiceImpl;
 
 public class Main {
     public static final String MAIN_PACKAGE_NAME = "mate.academy";
     private static final Injector injector =
             Injector.getInstance(MAIN_PACKAGE_NAME);
+
     public static void main(String[] args) {
-        MovieDao movieDao = (MovieDao) Injector.getInstance(MovieDao.class);
-        MovieService movieService = new MovieServiceImpl(movieDao);
+        MovieService movieService = (MovieService) Injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("Deadpool");
+        movie.setDescription("Good comedy!");
+        movieService.add(movie);
+
+        Movie movieFromDb = movieService.get(1L);
+        System.out.println(movieFromDb);
     }
 }
