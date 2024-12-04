20240e411f560e16e62635c3d0b54df5763a6d35
Epikano <epikano@gmail.com>
2024-12-04 12:14:15 +0200

    Added hibernate-configuration-hw solution.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        // Create a new movie
+        Movie movie = new Movie();
+        movie.setTitle("Inception");
+        movie.setDescription("A mind-bending thriller by Christopher Nolan.");
+
+        // Add the movie to the database
+        Movie savedMovie = movieService.add(movie);
+        System.out.println("Movie added: " + savedMovie.getTitle());
+
+        // Retrieve the movie by ID
+        Movie retrievedMovie = movieService.get(savedMovie.getId());
+        System.out.println("Retrieved movie: " + retrievedMovie.getTitle());
     }
 }
