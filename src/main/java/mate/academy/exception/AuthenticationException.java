be784f03e51f00dc57d478ae51a881f3fbc4af83
Mateusz Seler <mateusz.seler@gmail.com>
2024-12-06 12:31:42 +0100

    test version

--- src/main/java/mate/academy/exception/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.exception;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
