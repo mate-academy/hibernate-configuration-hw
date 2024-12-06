e2853b33e1aeab92c5e561025e3f5121b24ee010
Oleksii <obabych1@stu.vistula.edu.pl>
2024-12-06 18:11:46 +0100

    adding annotations

--- src/main/java/mate/academy/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieServiceImpl.java
@@ -3,9 +3,11 @@ package mate.academy.service.impl;
 import mate.academy.dao.MovieDao;
 import mate.academy.dao.impl.MovieDaoImpl;
 import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Service;
 import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
 
+@Service
 public class MovieServiceImpl implements MovieService {
     private final MovieDao movieDao = new MovieDaoImpl();
 
