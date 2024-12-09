423f417e47a327093885451b9a34afa9d9ffad3c
Mihail <mishadyakivnich@gmail.com>
2024-12-09 15:18:28 +0200

    resf

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -2,13 +2,12 @@ package mate.academy.dao;
 
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
-import mate.academy.model.Movie;
 import java.util.Optional;
+import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
