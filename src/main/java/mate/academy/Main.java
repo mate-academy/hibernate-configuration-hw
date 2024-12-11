f4de4dd6b7ed6c847ec68ffe5ea69054f99014aa
jekaGr <jekagrusha@gmail.com>
2024-12-11 18:01:38 +0200

    create hibernate-configuration

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+import mate.academy.service.MovieServiceImpl;
 
+public class Main {
+    private static final Injector injector = Injector
+            .getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = new MovieServiceImpl();
+        Movie movie = new Movie();
+        movie.setTitle("Fast and Furious 9");
+        movie.setDescription("Fast and Furious 9");
+        movieService.add(movie);
     }
 }
