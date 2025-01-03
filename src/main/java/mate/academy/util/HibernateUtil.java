5fa7ac7b6babd5c1131f90a1a16f7144372aade8
Alinka Lyn <alinka.lyn@ukr.net>
2025-01-03 21:11:12 +0200

    solved task

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -4,10 +4,11 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
     private HibernateUtil() {
     }
 
-    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         try {
@@ -25,4 +26,5 @@ public class HibernateUtil {
         getSessionFactory().close();
     }
 }
+
 
