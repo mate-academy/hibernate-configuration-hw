09fdf27dd08491e77cc89df54d0b0eb1d23942bd
homeworkfirst <zookotelif@gmail.com>
2025-01-14 23:58:40 +0200

    main changed, added Movie MovieDaoImpl MovieService MovieServiceImpl HibernateUtil

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("The Matrix");
+        movie.setDescription("A computer programmer discovers a mysterious world...");
+
+        movieService.add(movie);
+
+        movieService.get(movie.getId()).ifPresent(savedMovie ->
+                System.out.println("Found movie: " + savedMovie.getTitle()));
     }
 }
