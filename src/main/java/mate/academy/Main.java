1f2326650de5d60c6031c9aed3780035659de1fc
Yaroslav Olieniev <olieniev.od.ua@gmail.com>
2025-01-30 16:39:54 +0100

    added hibernate configuration and implementation of dao and service classes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
-
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService service = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Inception");
+        movie.setDescription("Movie about time!");
+        Movie addedMovie = service.add(movie);
+        System.out.println("Added movie: " + addedMovie);
+        Movie obtainedMovie = service.get(addedMovie.getId());
+        System.out.println("Obtained movie with id " + addedMovie.getId()
+                + ": " + obtainedMovie);
     }
 }
