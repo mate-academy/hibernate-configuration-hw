f9e1a7004e77ed58c1596b053e64a59e2274ea9e
Sergii Konovalov <sergii2konovalov@gmail.com>
2025-01-31 13:19:43 +0200

    hw-solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("King Kong");
+        movie.setDescription("Movie about monkey");
+        movieService.add(movie);
+        movieService.get(1L);
     }
 }
