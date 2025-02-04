379732fd59015274ba1c73012b44f2edb1f106a8
FroGitHub <andrey8102310@gmail.com>
2025-02-04 18:08:54 +0200

    add Movie API with Hibernate

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,24 @@
 package mate.academy;
 
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+import mate.academy.service.MovieServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
+
+        MovieService movieService = new MovieServiceImpl();
+
+        Movie movie = new Movie();
+
+        movie.setTitle("Marvel");
+        movie.setDescription("Marvel description");
+
+        movie = movieService.add(movie);
+
+        System.out.println(movie);
+
+        System.out.println(movieService.get(movie.getId()));
 
     }
 }
