a5c7a83fe0714e2c0fe1bf2996551d3f519e7f72
Kate Kraska <kate2002.kk42@gmail.com>
2025-02-05 16:34:48 +0200

    Initial commit for hw-solution

---
+++ src/main/java/mate/academy/model/MovieServiceImpl.java
@@ -0,0 +1,21 @@
+package mate.academy.model;
+
+import mate.academy.dao.MovieDao;
+
+public class MovieServiceImpl implements MovieService {
+    private final MovieDao movieDao;
+
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.movieDao = movieDao;
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id).orElseThrow();
+    }
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+}
