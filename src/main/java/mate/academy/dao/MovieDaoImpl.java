1913793f533a8898e736bd470b396ff39874f421
Kate Kraska <kate2002.kk42@gmail.com>
2025-02-08 13:44:18 +0200

    fix problem5

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.dao;
 
 import java.util.Optional;
+import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
@@ -15,13 +16,14 @@ public class MovieDaoImpl implements MovieDao {
         Transaction transaction = null;
         try {
             transaction = session.beginTransaction();
+
             session.persist(movie);
             session.getTransaction().commit();
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't add movie to database", e);
+            throw new DataProcessingException("Can't add movie to database");
         } finally {
             session.close();
         }
