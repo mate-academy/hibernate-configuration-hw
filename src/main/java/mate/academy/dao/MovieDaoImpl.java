f3569997bf1c7cbbe2d1cca752ee65232f7c1c34
Kate Kraska <kate2002.kk42@gmail.com>
2025-02-08 12:16:22 +0200

    Initial commit for hw-solution

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,11 +1,13 @@
 package mate.academy.dao;
 
+import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import java.util.Optional;
 
+@Dao
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
