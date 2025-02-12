6090d4b5845ceb4820cae4d70f8c415d4f16b53e
maaarynet <mashajazz.mj@gmail.com>
2025-02-12 16:00:01 +0200

    Movie, MoviDao and MovieService were created

---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,35 @@
+package mate.academy.service;
+
+import mate.academy.DataProcessingException;
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+    @Inject
+    private MovieDao movieDao;
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id).orElseThrow(
+                () -> new DataProcessingException("Movie with ID " + id + " not found", null)
+        );
+    }
+
+    @Override
+    public Movie update(Movie movie) {
+        return movieDao.update(movie);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return movieDao.delete(id);
+    }
+}
