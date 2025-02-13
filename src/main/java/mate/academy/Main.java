19fd422f01b1a29c4fa7c3ba1ff2913a83c2dea2
Pavlo Sukhanko <swtlouisiana@gmail.com>
2025-02-13 20:10:55 +0000

    implements hibernate configuration

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+
+        movie.setTitle("The Hobbit");
+        movie.setDescription("About Hobbit and dwarfs");
+
+        movieService.add(movie);
+        System.out.println(movieService.get(movie.getId()));
 
     }
 }
