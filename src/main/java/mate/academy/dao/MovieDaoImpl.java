9a8318358fae31a3fa807a83787aadc4a2f50259
eulu <anegamega@gmail.com>
2025-02-18 21:07:58 +0200

    Implement MovieDao

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,48 @@
+package mate.academy.dao;
+
+import mate.academy.model.Movie;
+import mate.academy.service.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.Optional;
+
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(movie);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Optional<Movie> findById(Long id) {
+        Session session = null;
+        Movie movie;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            movie = session.get(Movie.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot retrieve movie from db, movieId: " + id, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return Optional.ofNullable(movie);
+    }
+}
