193f08e107e917297bab5d5708f589801478fbae
eulu <anegamega@gmail.com>
2025-02-18 21:32:47 +0200

    Add Identity generation strategy

--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "movies")
 public class Movie {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
