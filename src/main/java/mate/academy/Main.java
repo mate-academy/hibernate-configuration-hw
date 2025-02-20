5209cef0208388b1d3686cc42c89a32068be1bd8
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-02-20 16:28:24 +0200

    make task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieDao movieDao = (MovieDao) injector.getInstance(MovieDao.class);
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
+        Movie movie = new Movie();
+        movie.setTitle("Longlegs");
+
+        movieService.add(movie);
+        Movie movieById = movieService.get(1L);
+
+        System.out.println(movieById);
     }
 }
