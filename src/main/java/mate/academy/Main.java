f966d005425e73feb891480f4a5d223b24bf1425
Serhii Senkiv <senkiv34@gmail.com>
2025-02-23 20:03:21 +0900

    Solution for hibernate-configuration-hw

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,16 @@
 package mate.academy;
 
+import mate.academy.entity.Movie;
+import mate.academy.lib.Injector;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie("Harry Potter", "Awesome movie");
+        System.out.println(movieService.add(movie));
+        System.out.println(movieService.get(3L));
 
     }
 }
---
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,48 @@
+package mate.academy.dao.impl;
+
+import java.util.Optional;
+import mate.academy.dao.MovieDao;
+import mate.academy.entity.Movie;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    public static final String ERROR_DURING_CREATION_OF_MOVIE = "Error during creation of movie.";
+    public static final String ERROR_DURING_RETRIEVING_MOVIE = "Error during retrieving movie.";
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+    @Override
+    public Movie add(Movie movie) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(movie);
+            transaction.commit();
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException(ERROR_DURING_CREATION_OF_MOVIE, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            Optional<Movie> result = Optional.ofNullable(session.get(Movie.class, id));
+            return result;
+        }
+    }
+}
