bb5ca6c2d8dcab7f17c166ce7679150fe7f3bb22
denDy1224 <denchik12327@gmail.com>
2025-02-24 23:27:30 +0100

    fix_code_style_failing-sort_imports

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -3,8 +3,8 @@ package mate.academy.dao;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
-import mate.academy.util.HibernateUtil;
 import java.util.Optional;
+import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
