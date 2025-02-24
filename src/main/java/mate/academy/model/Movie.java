d121c012828647e8c0bba05691c3e00989d481ce
denDy1224 <denchik12327@gmail.com>
2025-02-24 23:04:43 +0100

    add_hibernate_config

---
+++ src/main/java/mate/academy/model/Movie.java
@@ -0,0 +1,38 @@
+package mate.academy.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    private String description;
+}
