ed7ef612cc2f9a168ecac24df68a49fa7ed6e480
Karol Gajda <karolgaj97@interia.pl>
2025-02-25 13:30:26 +0100

    first solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("The Godfather");
+        movie.setDescription("A story about a New York mafia family. "
+                + "Aging Don Corleone wants to pass power to his son");
+        //add
+        movieService.add(movie);
+        //get
+        System.out.println(movieService.get(movie.getId()));
     }
 }
