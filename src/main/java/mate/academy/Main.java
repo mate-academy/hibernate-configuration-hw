82647ef920a0e535652ff14174b43be46f801ed9
denDy1224 <denchik12327@gmail.com>
2025-02-25 09:28:57 +0100

    fix_code_style_failing-sort_imports

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,21 +1,17 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
-import mate.academy.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import mate.academy.service.MovieService;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
         Movie movie = new Movie();
         movie.setTitle("Avengers");
-        session.save(movie);
-        transaction.commit();
-        session.close();
+        movieService.get(1L);
     }
 }
--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -4,15 +4,21 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    
-    private static final SessionFactory instance = initSessionFactory();
-    
-    public static SessionFactory initSessionFactory() {
-        return new Configuration().configure()
-                .buildSessionFactory();
+
+    private static final SessionFactory sessionFactory = initSessionFactory();
+
+    private HibernateUtil() {
     }
-    
+
+    private static SessionFactory initSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot create SessionFactory", e);
+        }
+    }
+
     public static SessionFactory getSessionFactory() {
-        return instance;
+        return sessionFactory;
     }
 }
