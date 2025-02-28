4965d9baed5cb8f4b38d4a8ff3607da2a4297a14
Vadym Dmytriv <vadymdmytriv@gmail.com>
2025-02-28 11:16:32 +0100

    Commit 0.1

---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,48 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert movie into DB ", e);
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        try {
+            Movie movie = session.get(Movie.class, id);
+            return Optional.ofNullable(movie);
+        } finally {
+            session.close();
+        }
+    }
+
+}
