6c9e308ca5392031cbca99250c7380aa8fb98bf1
Sasha Koval <kovalsana40@gmail.com>
2025-03-02 11:48:00 +0200

    I will solve mistakes

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,8 +1,8 @@
 package mate.academy.dao;
 
 import mate.academy.exception.DataProcessingException;
-import mate.academy.lib.Dao;
 import java.util.Optional;
+import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
