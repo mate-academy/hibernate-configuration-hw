a3f78d7022c3954360a19ac19807517980389748
skviropitek <skvyra.anton@gmail.com>
2025-03-09 14:15:21 +0100

    temporal solution

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,14 +1,12 @@
 package mate.academy.dao;
 
-
+import java.util.Optional;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
-import java.util.Optional;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
@@ -35,7 +33,7 @@ public class MovieDaoImpl implements MovieDao {
     }
 
     @Override
-    public Optional <Movie> get(Long id) {
+    public Optional<Movie> get(Long id) {
         Session session = null;
         try {
             session = HibernateUtil.getSessionFactory().openSession();
