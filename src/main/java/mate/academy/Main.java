d353e26b4d9a1508b5c4945d9bdfbd2eb711c960
Danylo Pomirchyi <pomircijdanilo@gmail.com>
2025-03-17 22:31:09 +0200

    cfg

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("Vikings");
+        movie.setDescription("Very kind Saga");
+        movieService.add(movie);
+
+        Movie retrievedMovie = movieService.get(movie.getId());
+        System.out.println("Retrieved movie: " + retrievedMovie);
     }
 }
