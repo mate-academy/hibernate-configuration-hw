c1edb4aa35d70aa8e09709890f90a82c628606d5
Yaroslav Pryshchepa <yrikpa200022@gmail.com>
2025-03-24 20:13:05 +0200

    Implemented class Movie as part of Hibernate project

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Inception");
+        movie.setDescription("A mind-bending thriller");
+        Movie savedMovie = movieService.add(movie);
+        System.out.println("Saved movie: " + savedMovie);
+        Movie fetchedMovie = movieService.get(savedMovie.getId());
+        System.out.println("Fetched movie: " + fetchedMovie);
     }
 }
