9a04eb01b811584907695b802ec9d2039f5c6bcc
Yaroslav Narozhnyi <gnredit9@gmail.com>
2025-03-26 00:45:40 +0200

    5

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,8 +1,8 @@
 package mate.academy.dao;
 
 import mate.academy.DataProcessingException;
-import mate.academy.lib.Dao;
 import java.util.Optional;
+import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
