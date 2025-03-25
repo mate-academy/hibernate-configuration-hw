afed484f3e122eb130058e2eeca50563ae5fc5ae
Geks <gekatonheyr@ukr.net>
2025-03-25 15:34:34 +0200

    Had worked under the homework: - added required hibernate dependencies; - configured hibernate; - created and implemented HibernateUtil class; - created Movie model class; - created MovieDao abstraction; - create MovieService abstraction; - created custom Exception; - annotated all necessary entities; - added test use cases to main() method.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Leon");
+        movie.setDescription("Awesome love story movie about little girl and best Cleaner.");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(movie);
+        System.out.println(movieService.get(1L));
 
     }
 }
