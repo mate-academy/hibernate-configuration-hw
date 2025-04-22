8af54e034df428ffbc4a3651c5aef57feb64283b
Tomasz Baran <MarcusOfficial@tuta.io>
2025-04-22 14:21:43 +0200

    1

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
+import mate.academy.dao.MovieDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieDao movieDao = (MovieDao) injector.getInstance(MovieDao.class);
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
+        Movie movie = new Movie();
+        movie.setTitle("test");
+
+        movieService.add(movie);
+        Movie movieById = movieService.get(1L);
+
+        System.out.println(movieById);
     }
 }
