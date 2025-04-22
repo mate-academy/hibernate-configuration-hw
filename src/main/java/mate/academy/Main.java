9f73246b1599db90b0a3297a4c74cdb7fcb30adb
Tomasz Baran <MarcusOfficial@tuta.io>
2025-04-22 17:26:14 +0200

    1

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
 
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Titanic");
+        movie.setDescription("Movie about bout");
+        Movie add = movieService.add(movie);
+        System.out.println("Added movie: " + add);
+        Movie movie1 = movieService.get(movie.getId());
+        System.out.println("Movie with id: " + movie.getId() + " : " + movie1);
     }
 }
