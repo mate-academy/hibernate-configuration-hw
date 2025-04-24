8e005f27219e3d2e7b363a7867b460b907fbc915
Mateusz <mateusz.kiryk@o2.pl>
2025-04-24 18:48:06 +0200

    First solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie inception = new Movie("Inception");
+        movieService.add(inception);
+        System.out.println("Added: " + inception);
+
+        Movie fetched = movieService.get(inception.getId());
+        System.out.println("Fetched: " + fetched);
+
+        mate.academy.util.HibernateUtil.shutdown();
     }
 }
---
+++ src/main/java/mate/academy/model/Movie.java
@@ -0,0 +1,37 @@
+package mate.academy.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    public Movie() {}
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{id=" + id + ", title='" + title + "'}";
+    }
+}
