1d44678195ef157eb578c22de4c51c1ebc7917b9
Mateusz <mateusz.kiryk@o2.pl>
2025-05-01 12:55:56 +0200

    Third - Final solution

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -1,20 +1,17 @@
 package mate.academy.util;
 
+import mate.academy.model.Movie;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import mate.academy.model.Movie;  // <-- dodaj import
 
 public final class HibernateUtil {
     private static final SessionFactory sessionFactory = initSessionFactory();
 
     private HibernateUtil() {
-        // zapobiega tworzeniu instancji
     }
 
     private static SessionFactory initSessionFactory() {
         try {
-            // Jeżeli chcesz mieć pewność, że encja zostanie wczytana,
-            // możesz dodać ją ręcznie:
             Configuration cfg = new Configuration().configure();
             cfg.addAnnotatedClass(Movie.class);
             return cfg.buildSessionFactory();
