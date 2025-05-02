5600e2562a702e13bf3ea85b81389e96ba27bab2
xMaXD <max260505.mx22@gmail.com>
2025-05-03 01:41:07 +0200

    fix4

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -4,16 +4,24 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static final SessionFactory instance = initSessionFactory();
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private HibernateUtil() {
     }
 
-    private static SessionFactory initSessionFactory() {
-        return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError("Initial SessionFactory creation failed: " + ex);
+        }
     }
 
     public static SessionFactory getSessionFactory() {
-        return instance;
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
     }
 }
