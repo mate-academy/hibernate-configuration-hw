f6b82c0c0a5927cb9fcd5e28d1f1dc94097b407d
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2025-05-04 21:12:03 +0200

    first commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("The Godfather");
+        movie.setDescription("Something bla bla");
+
+        Movie addedMovie = movieService.add(movie);
+        System.out.println("Added move: " + addedMovie);
+
+        Movie retrievedMovie = movieService.get(addedMovie.getId());
+        System.out.println("Retrieved movie: " + retrievedMovie);
 
     }
 }
+
